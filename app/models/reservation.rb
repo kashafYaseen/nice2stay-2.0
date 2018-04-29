3b95217e74793504ae68ae5e08bb50d77122c4d5
Ahmad Saleem <ahmadsaleem93@gmail.com>
2018-04-29 14:49:46 +0500

    Reindex transaction after changes to a reservations.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,10 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :transaction
+
+  after_commit :reindex_transaction
+
+  def reindex_transaction
+    transaction.reindex
+  end
 end
