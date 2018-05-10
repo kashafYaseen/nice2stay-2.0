25b927e1ae54763a29fcf382992892f9545a3b7a
Sikandar Tariq <sikandar.tariq@thedevden.co>
2018-05-10 17:16:59 +0500

    Add reservation dates validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,6 +3,7 @@ class Reservation < ApplicationRecord
   belongs_to :lodging
 
   validates :check_in, :check_out, presence: true
+  validate :availability
 
   after_create :update_lodging_availability
 
@@ -10,5 +11,11 @@ class Reservation < ApplicationRecord
 
     def update_lodging_availability
       lodging.availabilities.where(available_on: (check_in..check_out-1.day).map(&:to_s)).destroy_all
+    end
+
+    def availability
+      return unless check_in.present? && check_out.present?
+      available_days = lodging.availabilities.where(available_on: (check_in..check_out-1.day).map(&:to_s)).count
+      errors.add(:base, "lodging is not available for selected dates") if available_days < (check_out - check_in).to_i
     end
 end
