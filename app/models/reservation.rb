bb41905cc979c9f2a8b6db0c2928a8589ffe48c2
Sikandar Tariq <sikandar.tariq@thedevden.co>
2018-05-25 16:48:32 +0500

    Add validations for rules

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,11 +1,15 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :lodging
+  has_many :rules, through: :lodging
 
   validates :check_in, :check_out, presence: true
   validate :availability
+  validate :accommodation_rules
 
   after_create :update_lodging_availability
+
+  delegate :active, to: :rules, prefix: true, allow_nil: true
 
   private
 
@@ -17,5 +21,18 @@ class Reservation < ApplicationRecord
       return unless check_in.present? && check_out.present?
       available_days = lodging.availabilities.where(available_on: (check_in..check_out-1.day).map(&:to_s)).count
       errors.add(:base, "lodging is not available for selected dates") if available_days < (check_out - check_in).to_i
+    end
+
+    def accommodation_rules
+      return unless rules_active.present?
+      rules_active.each do |rule|
+        if rule.days_multiplier.present?
+          errors.add(:base, "Reserve days should be multiple of #{rule.days_multiplier}") unless (check_out - check_in).to_i % rule.days_multiplier == 0
+        end
+
+        if rule.check_in_days.present? & check_in.present?
+          errors.add(:base, "Check in day should be #{rule.check_in_days}") unless check_in.strftime("%A") == rule.check_in_days
+        end
+      end
     end
 end
--- app/models/rule.rb
+++ app/models/rule.rb
@@ -2,4 +2,6 @@ class Rule < ApplicationRecord
   belongs_to :lodging
 
   DAY_OF_WEEK = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+  scope :active, -> { where("(start_date is ? and end_date is ?) or (start_date <= ? and end_date > ?)", nil, nil, Date.today, Date.today) }
 end
