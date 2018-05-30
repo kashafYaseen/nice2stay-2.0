c88593fa70e280aae7dbf3978ea41dfcc96876d2
Ahmad Saleem <ahmadsaleem93@gmail.com>
2018-05-30 13:06:27 +0500

    Fix scope

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -3,5 +3,5 @@ class Rule < ApplicationRecord
 
   DAY_OF_WEEK = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
-  scope :active, -> { where("(start_date is ? and end_date is ?) or (start_date <= ? and end_date > ?)", nil, nil, Date.today, Date.today) }
+  scope :active, -> { where("(start_date is ? and end_date is ?) or (start_date >= ? and end_date > ?)", nil, nil, Date.current, Date.current) }
 end
