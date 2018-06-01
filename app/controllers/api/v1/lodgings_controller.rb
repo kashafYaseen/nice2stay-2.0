44485deafb88a94dc6d46fe7eeed0a7058cdd6d9
Sikandar Tariq <sikandar.tariq@thedevden.co>
2018-06-01 17:31:25 +0500

    Add create action

--- app/controllers/api/v1/lodgings_controller.rb
+++ app/controllers/api/v1/lodgings_controller.rb
@@ -5,8 +5,21 @@ class Api::V1::LodgingsController < Api::V1::ApiController
     return not_acceptable("Lodging not found") unless @lodging.present?
   end
 
+  def create
+    @lodging = Lodging.new(lodging_params)
+    if @lodging.save
+      render status: :created
+    else
+      render json: @lodging.errors, status: :unprocessable_entity
+    end
+  end
+
   private
     def set_lodging
       @lodging = Lodging.find_by(id: params[:id])
+    end
+
+    def lodging_params
+      params.require(:lodging).permit(:street, :city, :zip, :state, :beds, :baths, :sq__ft, :sale_date, :price, :latitude, :longitude, :adults, :children, :infants, :lodging_type)
     end
 end
