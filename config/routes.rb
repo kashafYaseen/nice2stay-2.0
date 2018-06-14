1129a40a08b82d70a7531b9ecdcecf3ee20be3ab
Ahmad Saleem <ahmadsaleem93@gmail.com>
2018-06-15 02:34:31 +0500

    Remove administrate

--- config/routes.rb
+++ config/routes.rb
@@ -13,12 +13,6 @@ Rails.application.routes.draw do
 
   get '/privacy', to: 'home#privacy'
   get '/terms', to: 'home#terms'
-  namespace :admin do
-    resources :users
-    resources :announcements
-
-    root to: "users#index"
-  end
 
   resources :announcements, only: [:index]
   authenticate :user, lambda { |u| u.admin? } do
