423f8c1985aaa9984a5c727f1065ff51d2f2abe9
Sikandar Tariq <sikandar.tariq@thedevden.co>
2018-06-22 16:26:31 +0500

    Add crm attributes to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_22_072202) do
+ActiveRecord::Schema.define(version: 2018_06_22_110048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -229,6 +229,13 @@ ActiveRecord::Schema.define(version: 2018_06_22_072202) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "setting", default: 0.0
+    t.float "quality", default: 0.0
+    t.float "interior", default: 0.0
+    t.float "communication", default: 0.0
+    t.float "service", default: 0.0
+    t.text "suggetion"
+    t.string "title"
     t.index ["lodging_id"], name: "index_reviews_on_lodging_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
