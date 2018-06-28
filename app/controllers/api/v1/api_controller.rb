50dccaa1d3b52902c2c70e83cf4e960865a60853
Sikandar Tariq <sikandar.tariq@thedevden.co>
2018-06-28 11:31:40 +0500

    Secure api with ip filter

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ApiController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_error
+  before_action :filter_ip
 
   protected
     def not_acceptable(errors)
@@ -10,5 +11,14 @@ class Api::V1::ApiController < ActionController::API
     def unprocessable_entity(errors)
       render json: { errors: errors }, status: :unprocessable_entity
       return
+    end
+
+    def not_authenticated
+      render json: { errors: ['IP not authorized'] }, status: :unauthorized
+      return
+    end
+
+    def filter_ip
+      not_authenticated unless ENV['CRM_REMOTE_IP'] == request.remote_ip
     end
 end
