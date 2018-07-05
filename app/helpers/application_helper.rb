f519bd0332f02da6950a8e2350b2951fcb1a0416
Ahmad Saleem <ahmadsaleem93@gmail.com>
2018-07-05 15:21:14 +0500

    Revert "added login"
    This reverts commit 4cc07be15e3e2b6425097cd35674e398837d64a8.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,4 @@
 module ApplicationHelper
-
- 
-  
   def bootstrap_class_for(flash_type)
     {
       success: "alert-success",
@@ -10,18 +7,4 @@ module ApplicationHelper
       notice: "alert-info"
     }.stringify_keys[flash_type.to_s] || flash_type.to_s
   end
-
-  
-  def resource_name
-    :user
-  end
- 
-  def resource
-    @resource ||= User.new
-  end
- 
-  def devise_mapping
-    @devise_mapping ||= Devise.mappings[:user]
-  end
-
 end
