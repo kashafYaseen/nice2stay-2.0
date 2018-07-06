19a49c5adeb2c716e527a92d29e9f4332986ea16
Sikandar Tariq <sikandar.tariq@thedevden.co>
2018-07-06 16:48:52 +0500

    Update per day price lodgic

--- app/models/price.rb
+++ app/models/price.rb
@@ -5,6 +5,8 @@ class Price < ApplicationRecord
 
   scope :of_child, -> (child_id) { joins(:availability).where('lodging_child_id = ?', child_id) }
 
+  delegate :available_on, to: :availability
+
   searchkick
 
   def search_data
@@ -13,5 +15,12 @@ class Price < ApplicationRecord
       lodging_id: lodging.id,
       lodging_child_id: lodging_child.id
     )
+  end
+
+  def day_price
+    return sunday_price if available_on.wday == 0 && sunday_price.present?
+    return friday_price if available_on.wday == 5 && friday_price.present?
+    return saturday_price if available_on.wday == 6 && saturday_price.present?
+    minimum_per_day_price || amount
   end
 end
