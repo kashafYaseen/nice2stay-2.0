f9fba44a59194216ec1dcc93432e907fc1470633
Sikandar Tariq <sikandar.tariq@thedevden.co>
2018-07-27 17:17:58 +0500

    Add action to remove cart items

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,17 +1,27 @@
 class CartsController < ApplicationController
   before_action :set_reservations
+  before_action :empty_cart, only: [:edit, :destroy]
 
   def show
   end
 
+  def edit
+    @reservations.find_by_id(params[:reservation_id]).try(:delete)
+    redirect_to carts_en_path, notice: 'Reservation was removed successfully.'
+  end
+
   def destroy
-    return unless @reservations.present?
-    redirect_to carts_en_path, notice: 'Cart was cleared successfully.' if @reservations.delete_all
+    @reservations.delete_all
+    redirect_to carts_en_path, notice: 'Cart was cleared successfully.'
   end
 
   private
     def set_reservations
       return unless current_user.present?
       @reservations = current_user.reservations_in_cart
+    end
+
+    def empty_cart
+      return redirect_to carts_en_path unless @reservations.present?
     end
 end
