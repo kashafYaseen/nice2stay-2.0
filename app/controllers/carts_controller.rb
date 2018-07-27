3ffbfd1c45dcf8962ac880afa2d8168dc6f4a22d
Sikandar Tariq <sikandar.tariq@thedevden.co>
2018-07-27 17:50:48 +0500

    Save reservation without user & add cart session values

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -7,18 +7,20 @@ class CartsController < ApplicationController
 
   def edit
     @reservations.find_by_id(params[:reservation_id]).try(:delete)
+    session[:reservations] = @reservations.ids.join(',') if session[:reservations].present?
     redirect_to carts_en_path, notice: 'Reservation was removed successfully.'
   end
 
   def destroy
     @reservations.delete_all
+    session.delete(:reservations) if session[:reservations].present?
     redirect_to carts_en_path, notice: 'Cart was cleared successfully.'
   end
 
   private
     def set_reservations
-      return unless current_user.present?
-      @reservations = current_user.reservations_in_cart
+      return @reservations = current_user.reservations_in_cart if current_user.present?
+      @reservations = Reservation.where(id: session[:reservations].split(',')) if session[:reservations].present?
     end
 
     def empty_cart
