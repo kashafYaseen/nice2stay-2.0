403ee253e97f4fb60275455c4d84192b6ea24b52
Sikandar Tariq <sikandar.tariq@thedevden.co>
2018-08-16 16:10:44 +0500

    Add searchkick to campaign

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,5 +1,13 @@
 class Campaign < ApplicationRecord
   has_and_belongs_to_many :regions
 
+  searchkick word_start: [:title]
+
   validates :title, :description, presence: true
+
+  def search_data
+    attributes.merge(
+      regions: regions.pluck(:name)
+    )
+  end
 end
