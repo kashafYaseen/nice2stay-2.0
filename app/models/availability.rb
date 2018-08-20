04a9ca5736c6990a05a25874cf6a3bacfbc7ff78
Sikandar Tariq <sikandar.tariq@thedevden.co>
2018-08-20 11:38:12 +0500

    Fix price range issue for boundry values

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -8,6 +8,7 @@ class Availability < ApplicationRecord
   accepts_nested_attributes_for :prices, allow_destroy: true
 
   scope :with_in, -> (from, to) { where('available_on > ? and available_on < ?', from, to) }
+  scope :for_range, -> (from, to) { where('available_on >= ? and available_on <= ?', from, to) }
 
   def reindex_lodging
     lodging.reindex
--- app/services/update_lodging_prices.rb
+++ app/services/update_lodging_prices.rb
@@ -23,7 +23,7 @@ class UpdateLodgingPrices
       prices.each do |price_range|
         Price.bulk_insert do |price|
           update_arrays(price_range)
-          lodging.availabilities_with_in(price_range[:from], price_range[:to]).each do |availability|
+          lodging.availabilities_for_range(price_range[:from], price_range[:to]).each do |availability|
             price.add(amount: day_price(price_range, availability.available_on), children: price_range[:children], adults: price_range[:adults],
               infants: price_range[:infants], minimum_stay: price_range[:minimal_stay], availability_id: availability.id, weekly_price: nil, created_at: Date.current, updated_at: Date.current)
 
