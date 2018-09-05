3b49ec2f488f41af6bf76ed930c40963d9c77888
Sikandar Tariq <sikandar.tariq@thedevden.co>
2018-09-05 16:39:08 +0500

    Add delegate for reservation requests

--- app/controllers/dashboard/reservations_controller.rb
+++ app/controllers/dashboard/reservations_controller.rb
@@ -2,6 +2,6 @@ class Dashboard::ReservationsController < DashboardController
   def index
     @title = 'Reservations'
     add_breadcrumb @title, dashboard_reservations_path
-    @reservations = current_user.reservations.requests
+    @reservations = current_user.reservations_requests
   end
 end
