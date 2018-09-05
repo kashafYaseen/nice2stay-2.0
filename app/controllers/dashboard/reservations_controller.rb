581448e02f9536c07cb2ab7aa8a2bd6622a73794
Sikandar Tariq <sikandar.tariq@thedevden.co>
2018-09-05 16:35:07 +0500

    Set title & breadcrums

--- app/controllers/dashboard/reservations_controller.rb
+++ app/controllers/dashboard/reservations_controller.rb
@@ -1,6 +1,7 @@
 class Dashboard::ReservationsController < DashboardController
-
   def index
+    @title = 'Reservations'
+    add_breadcrumb @title, dashboard_reservations_path
     @reservations = current_user.reservations.requests
   end
 end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,9 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
   layout "dashboard"
+  add_breadcrumb "Dashboard", :dashboard_path
 
   def index
+    @title = 'Dashboard'
   end
 end
