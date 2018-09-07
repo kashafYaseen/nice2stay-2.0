21598424a262a9599aa5e08f7a232eeadc550f90
Sikandar Tariq <sikandar.tariq@thedevden.co>
2018-09-07 16:28:05 +0500

    Add model experience

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_07_110810) do
+ActiveRecord::Schema.define(version: 2018_09_07_112454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -138,6 +138,16 @@ ActiveRecord::Schema.define(version: 2018_09_07_110810) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["lodging_id"], name: "index_discounts_on_lodging_id"
+  end
+
+  create_table "experiences", force: :cascade do |t|
+    t.string "name"
+    t.string "tag"
+    t.string "slug"
+    t.text "short_desc"
+    t.boolean "publish"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "friendly_id_slugs", id: :serial, force: :cascade do |t|
