fb2d5f07ba51be2443b5aa59e0cb4856afe7f91a
Sikandar Tariq <sikandar.tariq@thedevden.co>
2018-09-14 11:54:38 +0500

    Add images attribute to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_11_080132) do
+ActiveRecord::Schema.define(version: 2018_09_14_065313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -399,6 +399,8 @@ ActiveRecord::Schema.define(version: 2018_09_11_080132) do
     t.text "suggetion"
     t.string "title"
     t.bigint "reservation_id"
+    t.string "thumbnails", default: [], array: true
+    t.string "images", default: [], array: true
     t.index ["lodging_id"], name: "index_reviews_on_lodging_id"
     t.index ["reservation_id"], name: "index_reviews_on_reservation_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
