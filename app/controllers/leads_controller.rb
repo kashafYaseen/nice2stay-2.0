e0dd3589afa8dec85b91722c7427fdbe4ce31d80
Sikandar Tariq <sikandar.tariq@thedevden.co>
2018-09-19 11:12:31 +0500

    Refactor create_user method of leads controller

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,12 +1,15 @@
 class LeadsController < ApplicationController
+  before_action :set_user
+
   def create
-    create_user unless current_user.present?
     @lead = @user.leads.build(lead_params)
     return redirect_to root_en_path, notice: "Lead was created successfully." if @lead.save && @user.valid?
   end
 
   private
-    def create_user
+    def set_user
+      return @user = current_user if current_user.present?
+
       if params[:create_account].present?
         @user = User.with_login.new(user_params)
       else
@@ -14,7 +17,6 @@ class LeadsController < ApplicationController
         @user.attributes = user_params
         @user.password = @user.password_confirmation = Devise.friendly_token[0, 20]
       end
-      return true if @user.save
     end
 
     def user_params
