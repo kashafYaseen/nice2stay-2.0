b8d4a03146313c4014efe6bdcbddec2155cad876
Sikandar Tariq <sikandar.tariq@thedevden.co>
2018-09-24 15:44:58 +0500

    Add bullet gem for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,13 @@ config.webpacker.check_yarn_integrity = true
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+    Bullet.add_footer = true
+  end
 end
