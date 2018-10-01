573da405b956a6c45ea8ac5c6a67966c4f81db07
Sikandar Tariq <sikandar.tariq@thedevden.co>
2018-10-01 16:08:35 +0500

    Add active_admin reservation model

---
+++ app/admin/reservations.rb
@@ -0,0 +1,28 @@
+ActiveAdmin.register Reservation do
+  remove_filter :review, :rules, :prices, :total_price, :discount, :rent, :crm_booking, :cleaning_cost
+
+  controller do
+    def permitted_params
+      params.permit!
+    end
+
+    def scoped_collection
+      Reservation.includes(:user, { lodging: :translations })
+    end
+  end
+
+  index do
+    selectable_column
+    id_column
+    column :user
+    column :lodging
+    column :check_in
+    column :check_out
+    column :booking_status
+    column :request_status
+    column :rent
+    column :in_cart
+
+    actions
+  end
+end
