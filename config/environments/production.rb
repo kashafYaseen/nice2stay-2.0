1fab0fc7f7251532d5274120963e626f75c0735e
Sikandar Tariq <sikandar.tariq@thedevden.co>
2018-10-03 15:35:33 +0500

    Add host url for production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,8 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  config.action_mailer.default_url_options = { host: "http://www.bestsecretplaces.com" }
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
