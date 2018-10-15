9358491c2ad43c079451198a1f8b072b1efe96c3
Sikandar Tariq <sikandar.tariq@thedevden.co>
2018-10-15 17:05:53 +0500

    Add sign-up and edit profile forms

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,11 +1,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   protected
     def sign_up_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:first_name, :last_name, :email, :city, :address, :country, :zipcode, :phone, :password, :password_confirmation)
     end
 
     def account_update_params
-      params.require(:user).permit(:first_name, :last_name, :image, :email, :password, :password_confirmation, :current_password)
+      params.require(:user).permit(:first_name, :last_name, :image, :email, :city, :address, :country, :zipcode, :phone, :password, :password_confirmation, :current_password)
     end
 
     def after_update_path_for(resource)
