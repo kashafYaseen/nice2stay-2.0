8262db1d8447951d89f4f98f2eb5abb353d3c340
Sikandar Tariq <sikandar.tariq@thedevden.co>
2018-10-15 17:06:44 +0500

    Add validations for address fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ApplicationRecord
   delegate :active, to: :wishlists, allow_nil: true, prefix: true
   delegate :in_cart, :confirmed, to: :bookings, allow_nil: true, prefix: true
 
-  validates :first_name, :last_name, presence: true
+  validates :first_name, :last_name, :city, :address, :country, :zipcode, :phone, presence: true
   before_validation :set_password
 
   enum creation_status: {
