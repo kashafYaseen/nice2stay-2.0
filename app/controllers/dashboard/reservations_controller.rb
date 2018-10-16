0b37b573ae01d95b5af10f8a081f51d84c9444e5
Sikandar Tariq <sikandar.tariq@thedevden.co>
2018-10-16 14:47:13 +0500

    Update option requests flow

--- app/controllers/dashboard/reservations_controller.rb
+++ app/controllers/dashboard/reservations_controller.rb
@@ -1,4 +1,6 @@
 class Dashboard::ReservationsController < DashboardController
+  before_action :set_option, except: [:index]
+
   def index
     @title = 'Reservations'
     add_breadcrumb @title, dashboard_reservations_path
@@ -7,4 +9,22 @@ class Dashboard::ReservationsController < DashboardController
     @requests = current_user.reservations_non_confirmed.includes(lodging: :translations).page(params[:requests_page]).per(5)
     @options = current_user.reservations_confirmed_options.includes(lodging: :translations).page(params[:options_page]).per(5)
   end
+
+  def destroy
+    @option.destroy
+    redirect_to dashboard_reservations_path, notice: 'Option was removed successfully'
+  end
+
+  def accept_option
+    if @option.update(request_status: :pending, booking_status: :prebooking)
+      redirect_to dashboard_reservations_path, notice: 'Option status was updated successfully'
+    else
+      redirect_to dashboard_reservations_path, notice: 'Unable to process your request at the moment.'
+    end
+  end
+
+  private
+    def set_option
+      @option = current_user.reservations_confirmed_options.find(params[:id])
+    end
 end
---
+++ app/jobs/send_reservation_removal_details_job.rb
@@ -0,0 +1,7 @@
+class SendReservationRemovalDetailsJob < ApplicationJob
+  queue_as :default
+
+  def perform(reservation_id, crm_booking_id, booking_id)
+    SendBookingDetails.call(reservation_id, crm_booking_id, Booking.find_by(id: booking_id))
+  end
+end
