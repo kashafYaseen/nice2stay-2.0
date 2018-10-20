98b2b0cf00de4e0f49f0a7544ce811c869d959c7
Ahmad Saleem <ahmadsaleem93@gmail.com>
2018-10-20 12:10:49 +0500

    Revert "Try fix"
    This reverts commit 8cfaffea44639a6d4432e7b1fa4a5eda941de735.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class DashboardController < ApplicationController
+  protect_from_forgery prepend: true, with: :exception
+
   before_action :authenticate_user!
   layout "dashboard"
   add_breadcrumb "Dashboard", :dashboard_path
