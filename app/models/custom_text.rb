050493bfd4689e3ea63ab38ff3d69ec9d8a6a2fc
Sikandar Tariq <sikandar.tariq@thedevden.co>
2018-10-24 11:56:07 +0500

    Add custom text translation table

--- app/models/custom_text.rb
+++ app/models/custom_text.rb
@@ -1,2 +1,11 @@
 class CustomText < ApplicationRecord
+  translates :h1_text, :p_text, :meta_title, :meta_description, :redirect_url, :country, :region, :category, :experience
+
+  def seo_path
+    path = ""
+    path += "/#{country}" if country?
+    path += "/#{region}" if region?
+    path += "/#{category}" if category?
+    path += "/#{experience}" if experience?
+  end
 end
