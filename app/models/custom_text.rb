7b2bc357a93265059d327e47388e3bea5454c51d
Sikandar Tariq <sikandar.tariq@thedevden.co>
2018-10-26 11:00:22 +0500

    Update custom text model

--- app/models/custom_text.rb
+++ app/models/custom_text.rb
@@ -1,13 +1,28 @@
 class CustomText < ApplicationRecord
-  translates :h1_text, :p_text, :meta_title, :meta_description, :country, :region, :category, :experience
+  belongs_to :country
+  belongs_to :region
+  belongs_to :experience
+
+  translates :h1_text, :p_text, :meta_title, :meta_description, :category
+
+  delegate :slug, to: :country, prefix: true, allow_nil: true
+  delegate :slug, to: :region, prefix: true, allow_nil: true
+  delegate :slug, to: :experience, prefix: true, allow_nil: true
 
   def seo_path(locale)
-    translation = translations.find_by(locale: locale) || self
     path = ""
-    path += "/#{translation.country}" if translation.country?
-    path += "/#{translation.region}" if translation.region?
+    path += "/#{translated_slug country, locale}" if country.present?
+    path += "/#{translated_slug region, locale}" if region.present?
+
+    translation = translations.find_by(locale: locale) || self
     path += "/#{translation.category}" if translation.category?
-    path += "/#{translation.experience}" if translation.experience?
+
+    path += "/#{translated_slug experience, locale}" if experience.present?
     path
+  end
+
+  def translated_slug object, locale
+    translation = object.translations.find_by(locale: locale) || object
+    translation.slug
   end
 end
