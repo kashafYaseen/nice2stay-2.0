4cd72a651714da1cedf79c5eb8d9f12043955d4a
Sikandar Tariq <sikandar.tariq@thedevden.co>
2018-11-29 12:53:58 +0500

    Update custom-text active-admin controller

--- app/admin/custom_texts.rb
+++ app/admin/custom_texts.rb
@@ -3,5 +3,29 @@ ActiveAdmin.register CustomText do
     def permitted_params
       params.permit!
     end
+
+    def scoped_collection
+      CustomText.includes(:translations, :country, :region, :experience)
+    end
+  end
+
+  index do
+    selectable_column
+    id_column
+    column :translations do |custom_text|
+      custom_text.translations.count
+    end
+
+    column :seo_path
+    column :country
+    column :region
+    column :experience
+    column :home_page
+    column :country_page
+    column :region_page
+    column :navigation_popular
+    column :navigation_country
+
+    actions
   end
 end
