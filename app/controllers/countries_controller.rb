cd7bdde49688345b95ec9ea09e000f98320bbaf9
Sikandar Tariq <sikandar.tariq@thedevden.co>
2018-12-20 18:07:38 +0500

    Fix prices for spotlights

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -5,6 +5,7 @@ class CountriesController < ApplicationController
   def show
     @country = Country.friendly.find(params[:id])
     @lodgings = @country.lodgings_country_page
+    @lodgings.map{|lodging| lodging.cumulative_price(params.clone)}
     @custom_texts = @country.custom_texts.country_page
     @title = @country.meta_title
   end
--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -3,6 +3,7 @@ class RegionsController < ApplicationController
     @region = Country.friendly.find(params[:country_id]).regions.friendly.find(params[:id])
     @reservation = Reservation.new
     @lodgings = @region.lodgings_region_page
+    @lodgings.map{|lodging| lodging.cumulative_price(params.clone)}
     @custom_texts = @region.custom_texts.region_page
     @title = @region.meta_title
   end
