c5237d72e05e77d941c66873f40f1ff2910c487f
Sikandar Tariq <sikandar.tariq@thedevden.co>
2019-01-03 17:22:36 +0500

    Fix reservation removal job

--- app/jobs/send_reservation_removal_details_job.rb
+++ app/jobs/send_reservation_removal_details_job.rb
@@ -2,6 +2,6 @@ class SendReservationRemovalDetailsJob < ApplicationJob
   queue_as :default
 
   def perform(reservation_id, crm_booking_id, booking_id)
-    SendBookingDetails.call(reservation_id, crm_booking_id, Booking.find_by(id: booking_id))
+    SendReservationRemovalDetails.call(reservation_id, crm_booking_id, Booking.find_by(id: booking_id))
   end
 end
