d67578026bc432b210837378bf37bc8a5d1a092a
Sikandar Tariq <sikandar.tariq@thedevden.co>
2019-01-25 18:13:15 +0500

    Update admin reviews controller

--- app/admin/review.rb
+++ app/admin/review.rb
@@ -1,31 +1,3 @@
 ActiveAdmin.register Review do
-  menu false
-  actions :all, except: [:index, :new, :create]
-  permit_params :stars, :description, :user
-
-  form do |f|
-    inputs 'Specification' do
-      f.input :lodging_id, input_html: { disabled: true }
-      f.input :user
-      f.input :stars
-      f.input :description
-    end
-
-    f.actions do
-      f.action :submit
-    end
-  end
-
-  show do
-    attributes_table do
-      row :lodging
-      row :user
-      row :stars
-      row :description
-      row :created_at
-      row :updated_at
-    end
-
-    active_admin_comments
-  end
+  permit_params :stars, :setting, :quality, :interior, :communication, :service, :suggetion, :title, :published, :perfect, :anonymous
 end
