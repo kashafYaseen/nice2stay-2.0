20c3fa87d90fbdb26e9a5a042d0ae476225865f5
Sikandar Tariq <sikandar.tariq@thedevden.co>
2019-02-26 15:25:17 +0500

    Try fix for nil price error

--- app/services/update_lodging_prices.rb
+++ app/services/update_lodging_prices.rb
@@ -26,11 +26,11 @@ class UpdateLodgingPrices
           Price.bulk_insert do |price|
             update_arrays(price_range)
             lodging.availabilities_for_range(price_range[:from], price_range[:to]).each do |availability|
-              price.add(amount: day_price(price_range, availability.available_on), children: price_range[:children], adults: price_range[:adults],
+              price.add(amount: day_price(price_range, availability.available_on).to_f, children: price_range[:children], adults: price_range[:adults],
                 infants: price_range[:infants], minimum_stay: price_range[:minimal_stay], availability_id: availability.id, weekly_price: nil, created_at: Date.current, updated_at: Date.current)
 
               if price_range[:weekly_price].present?
-                price.add(amount: price_range[:amount], children: price_range[:children], adults: price_range[:adults],
+                price.add(amount: price_range[:amount].to_f, children: price_range[:children], adults: price_range[:adults],
                   infants: price_range[:infants], minimum_stay: ['7'], availability_id: availability.id, weekly_price: nil, created_at: Date.current, updated_at: Date.current)
               end
             end
