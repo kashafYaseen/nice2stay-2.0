aa61b0bf948433892b4816e6b1008b2574aef72f
Sikandar Tariq <sikandar.tariq@thedevden.co>
2019-03-22 12:32:13 +0500

    Add couple of translations for index page

--- app/helpers/lodgings_helper.rb
+++ app/helpers/lodgings_helper.rb
@@ -95,7 +95,7 @@ module LodgingsHelper
   end
 
   def render_price price, dynamic
-    return "<h3 class='price'>#{render_rounded_price price}</h3><p class='price-text nights-text'> for #{(params[:check_out].to_date - params[:check_in].to_date).to_i} #{t('nav_cart.nights').downcase}</p>".html_safe if dynamic
-    "<div class='price-text'> From </div> <h3 class='price'>#{render_rounded_price price}</h3><p class='price-text nights-text'> per #{t('search.night')}</p>".html_safe
+    return "<h3 class='price'>#{render_rounded_price price}</h3><p class='price-text nights-text'> #{t('search.for')} #{(params[:check_out].to_date - params[:check_in].to_date).to_i} #{t('nav_cart.nights').downcase}</p>".html_safe if dynamic
+    "<div class='price-text'> #{t('search.from')} </div> <h3 class='price'>#{render_rounded_price price}</h3><p class='price-text nights-text'> per #{t('search.night')}</p>".html_safe
   end
 end
