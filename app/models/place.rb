e4fb7475df0af9e3be060f3084b9c13683b2114a
Sikandar Tariq <sikandar.tariq@thedevden.co>
2019-04-22 14:43:51 +0500

    Add searchkick to place

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,8 +3,34 @@ class Place < ApplicationRecord
   belongs_to :region
   belongs_to :place_category
 
+  searchkick locations: [:location]
+
   extend FriendlyId
   friendly_id :name, use: :slugged
 
   translates :details, :description, :name, :slug
+
+  def search_data
+    attributes.merge(
+      location: { lat: latitude, lon: longitude },
+    )
+  end
+
+  def feature
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [longitude, latitude]
+      },
+      properties: {
+        id: id,
+        title: name,
+        description: description,
+        image: images.try(:first),
+        'marker-color': '#7D3C98',
+        'marker-size': 'small',
+      }
+    }
+  end
 end
