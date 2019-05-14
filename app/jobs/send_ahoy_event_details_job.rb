ec45634454a061d1c9131c0b782209f4f9cbf3f9
Sikandar Tariq <sikandar.tariq@thedevden.co>
2019-05-14 13:25:42 +0500

    Add guard condition for SendAhoyEventDetailsJob

--- app/jobs/send_ahoy_event_details_job.rb
+++ app/jobs/send_ahoy_event_details_job.rb
@@ -2,6 +2,7 @@ class SendAhoyEventDetailsJob < ApplicationJob
   queue_as :default
 
   def perform(event_id)
-    SendAhoyEventDetails.call(Ahoy::Event.find_by(id: event_id))
+    event = Ahoy::Event.find_by(id: event_id)
+    SendAhoyEventDetails.call(event) if event.present?
   end
 end
