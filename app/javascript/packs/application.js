6c284f7319f7de9990a86004bf5bd57274f57a07
Sikandar Tariq <sikandar.tariq@thedevden.co>
2019-05-17 16:07:17 +0500

    Add vue component guets-dropdown

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,19 +10,22 @@
 import Vue from 'vue/dist/vue.esm.js'
 import Datepicker from '../vue_components/datepicker.vue'
 import ReservationDatepicker from '../vue_components/reservation_datepicker.vue'
+import GuestsDropdown from '../vue_components/guests_dropdown.vue'
 import TurbolinksAdapter from 'vue-turbolinks'
 import AirbnbStyleDatepicker from 'vue-airbnb-style-datepicker'
+import NumberInputSpinner from 'vue-number-input-spinner'
 import 'vue-airbnb-style-datepicker/dist/vue-airbnb-style-datepicker.min.css'
 
 Vue.use(TurbolinksAdapter)
 Vue.use(AirbnbStyleDatepicker, { colors: { disabled: '#e2dede' } })
+Vue.component('number-input-spinner', NumberInputSpinner)
 
 window.initDatePicker = function() {
   if ($('.vue-app').length) {
     const vues = document.querySelectorAll(".vue-app");
     vues.forEach(function(element) {
       if(element.__vue__ == undefined) {
-        new Vue({ el: element, components: { ReservationDatepicker, Datepicker } })
+        new Vue({ el: element, components: { ReservationDatepicker, Datepicker, GuestsDropdown } })
       }
     });
   }
