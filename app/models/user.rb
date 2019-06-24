1cd32240821252136e50a815f8f94812c8b30dce
Sikandar Tariq <sikandar.tariq@thedevden.co>
2019-06-24 14:37:03 +0500

    Remove validation for zipcode

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: { message: "has an account. Click here to <input type='button' name='login-form' value='Login' class='btn btn-link btn-danger btn-sm' data-toggle='modal' data-target='#login-form-modal'>or here to <input type='button' name='reset-password-form' value='Reset password' class='btn btn-link btn-danger btn-sm' data-toggle='modal' data-target='#reset-pass-form-modal'>" }, allow_blank: true
   validates :first_name, :last_name, :phone, presence: true, unless: :encrypted_password_changed?
-  validates :city, :address, :country, :zipcode, presence: true, unless: :skip_validations?
+  validates :city, :address, :country, presence: true, unless: :skip_validations?
   before_validation :set_password
 
   attr_accessor :skip_validations
