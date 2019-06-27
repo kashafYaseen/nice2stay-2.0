2bdf8c2bd33721b0e5f663f8f718f202b55bbf57
Sikandar Tariq <sikandar.tariq@thedevden.co>
2019-06-27 15:06:31 +0500

    Add omniauth controller for google auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
 
   belongs_to :country, optional: true
   has_many :reviews
@@ -74,6 +75,11 @@ class User < ApplicationRecord
     hash << ENV['FRONT_CHAT_SECRET']
     hash << email
     hash.hexdigest
+  end
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    User.find_by(email: data['email']) || User.create(first_name: data['first_name'], last_name: data['last_name'], email: data['email'], password: Devise.friendly_token[0,20], image: data['image'])
   end
 
   private
