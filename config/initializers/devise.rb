26b474d819c915188aa02801bd7c090e5ea2c62b
Sikandar Tariq <sikandar.tariq@thedevden.co>
2019-06-28 11:57:53 +0500

    add instagram authorization

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -252,6 +252,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], { scope: 'userinfo.email, userinfo.profile' }
+  config.omniauth :instagram, ENV['INSTAGRAM_CLIENT_ID'], ENV['INSTAGRAM_CLIENT_SECRET']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
