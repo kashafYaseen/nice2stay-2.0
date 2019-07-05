5bd79dd9e11be276994203be5cee453a747dbb8a
Sikandar Tariq <sikandar.tariq@thedevden.co>
2019-07-05 10:44:24 +0500

    Add confirmation fields to social logins

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_27_110755) do
+ActiveRecord::Schema.define(version: 2019_07_04_115239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -795,6 +795,10 @@ ActiveRecord::Schema.define(version: 2019_06_27_110755) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_social_logins_on_confirmation_token", unique: true
     t.index ["user_id"], name: "index_social_logins_on_user_id"
   end
 
