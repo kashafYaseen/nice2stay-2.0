b19446e98665d517cfdac04cb6f9133dbb2de156
Sikandar Tariq <sikandar.tariq@thedevden.co>
2019-08-28 14:34:16 +0500

    Improve readability of guest centric offers service

--- app/services/get_guest_centric_offers.rb
+++ app/services/get_guest_centric_offers.rb
@@ -10,14 +10,13 @@ class GetGuestCentricOffers
   def initialize(lodging, params)
     @lodging = lodging
     @params = params
-    @uri = URI.parse("http://secure.guestcentric.net/api/secure/beapi/hotel/find_offers?#{query_params}")
+    @uri = URI.parse("http://secure.guestcentric.net/api/secure/beapi/hotel/find_offers")
   end
 
   def call
     http = Net::HTTP.new(uri.host, uri.port)
-    # http.use_ssl = true if Rails.env.production?
     request = Net::HTTP::Post.new(uri.request_uri, header)
-    request.body = param_details.to_json
+    request.set_form_data form_data
     JSON.parse http.request(request).body
   end
 
@@ -26,12 +25,19 @@ class GetGuestCentricOffers
       { 'username': ENV['GUEST_CENTRIC_USER'], 'pass': ENV['GUEST_CENTRIC_PASS'] }
     end
 
-    def param_details
-      { }
-    end
-
-    def query_params
-      "HotelCode=#{lodging.guest_centric_id}&checkIn=#{params[:check_in]}&nrNights=#{nights}&nrAdults=#{params[:adults].to_i}&nrChildren=#{params[:children].to_i}&nrRooms=#{params[:rooms].to_i}&languageCode=#{params[:locale]}&currency=EUR&key=#{ENV['GUEST_CENTRIC_KEY']}&includeCancelPolicies=1"
+    def form_data
+      {
+        'HotelCode': lodging.guest_centric_id,
+        "checkIn": params[:check_in],
+        'nrNights': nights,
+        'nrAdults': params[:adults].to_i,
+        'nrChildren': params[:children].to_i,
+        'nrRooms': params[:rooms].to_i,
+        'languageCode': params[:locale],
+        'currency': 'EUR',
+        'key': ENV['GUEST_CENTRIC_KEY'],
+        'includeCancelPolicies': 1,
+      }
     end
 
     def nights
