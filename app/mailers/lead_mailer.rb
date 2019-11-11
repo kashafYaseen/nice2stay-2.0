2d4fe906fbd4d6c85cbd982e48e4d108e1bc8fc1
Sikandar Tariq <sikandar.tariq@thedevden.co>
2019-11-11 16:29:40 +0500

    Minor fixes for leads mailer

--- app/mailers/lead_mailer.rb
+++ app/mailers/lead_mailer.rb
@@ -1,13 +1,18 @@
 class LeadMailer < ApplicationMailer
   def send_offers(user_id, lead_id)
-    @send_us_email = "mailto:finance@nice2stay.com"
     @user = User.find_by_id(user_id)
     @lead = @user.leads.find_by(id: lead_id)
+    @owner = @lead.admin_user
 
     local = @user.language.presence || :nl
 
     I18n.with_locale(local.downcase) do
-      make_bootstrap_mail(to: @user.email, subject: "Special offers for you")
+      make_bootstrap_mail(
+        from: @owner.email,
+        to: @user.email,
+        cc: "support@nice2stay.com",
+        subject: "Special offers for you"
+      )
     end
   end
 end
