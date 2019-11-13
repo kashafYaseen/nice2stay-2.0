0c6c63aea12283c4eaab118fed873e86e319901e
Sikandar Tariq <sikandar.tariq@thedevden.co>
2019-11-13 16:55:02 +0500

    Add action cumulative_price for api lodgings controller

--- app/controllers/api/v2/lodgings_controller.rb
+++ app/controllers/api/v2/lodgings_controller.rb
@@ -3,12 +3,17 @@ class Api::V2::LodgingsController < Api::V2::ApiController
 
   def index
     @lodgings = SearchLodgings.call(params)
-    @lodgings.map{ |lodging| lodging.cumulative_price(params.clone) }
     render json: Api::V2::LodgingSerializer.new(@lodgings).serialized_json, status: :ok
   end
 
   def show
     render json: Api::V2::LodgingSerializer.new(@lodging, { params: { rooms: true } }).serialized_json, status: :ok
+  end
+
+  def cumulative_price
+    @lodgings = Lodging.where(id: params[:ids].try(:split, ','))
+    @lodgings.map{ |lodging| lodging.cumulative_price(params.clone) }
+    render json: Api::V2::LodgingSerializer.new(@lodgings).serialized_json, status: :ok
   end
 
   private
--- config/routes/api_v2.rb
+++ config/routes/api_v2.rb
@@ -1,7 +1,9 @@
 namespace :api do
   namespace :v2 do
-    resources :lodgings
     resource :profiles, only: [:create]
     resource :sessions, only: [:create, :update]
+    resources :lodgings, only: [:index, :show] do
+      get :cumulative_price, on: :collection
+    end
   end
 end
