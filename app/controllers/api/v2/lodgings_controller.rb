2caeaf0ad6daadfe9bb2fc17ab00a2c50ef3bf44
Sikandar Tariq <sikandar.tariq@thedevden.co>
2019-11-13 16:36:04 +0500

    Update api lodgings controller & serializer

--- app/controllers/api/v2/lodgings_controller.rb
+++ app/controllers/api/v2/lodgings_controller.rb
@@ -3,11 +3,12 @@ class Api::V2::LodgingsController < Api::V2::ApiController
 
   def index
     @lodgings = SearchLodgings.call(params)
+    @lodgings.map{ |lodging| lodging.cumulative_price(params.clone) }
     render json: Api::V2::LodgingSerializer.new(@lodgings).serialized_json, status: :ok
   end
 
   def show
-    render json: Api::V2::LodgingSerializer.new(@lodging).serialized_json, status: :ok
+    render json: Api::V2::LodgingSerializer.new(@lodging, { params: { rooms: true } }).serialized_json, status: :ok
   end
 
   private
--- app/serializers/api/v2/lodging_serializer.rb
+++ app/serializers/api/v2/lodging_serializer.rb
@@ -1,7 +1,15 @@
 class Api::V2::LodgingSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name, :summary, :title, :subtitle, :street, :city,
-             :zip, :state, :latitude, :longitude, :beds, :baths, :sq__ft,
-             :sale_date, :price, :lodging_type, :adults, :children, :infants,
-             :description, :short_desc, :images, :created_at, :updated_at
+  attributes :id, :name, :h1, :h2, :lodging_type, :presentation,
+             :address, :latitude, :longitude, :adults, :children, :infants,
+             :price, :calculated_price, :dynamic_price, :summary, :description, :short_desc,
+             :images, :thumbnails, :average_rating, :created_at, :updated_at
+
+  attribute :total_reviews do |lodging|
+    lodging.all_reviews.count
+  end
+
+  attributes :rooms, if: Proc.new { |lodging, params| params.present? && params[:rooms].present? } do |lodging, params|
+    Api::V2::LodgingSerializer.new(lodging.lodging_children) if lodging.as_parent?
+  end
 end
