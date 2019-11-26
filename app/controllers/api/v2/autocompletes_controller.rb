04b2f23098985202fbf0408574ddfb74a14f7a12
Sikandar Tariq <sikandar.tariq@thedevden.co>
2019-11-26 17:26:10 +0500

    Refactor autocomplete request for mobile api

--- app/controllers/api/v2/autocompletes_controller.rb
+++ app/controllers/api/v2/autocompletes_controller.rb
@@ -1,10 +1,22 @@
 class Api::V2::AutocompletesController < Api::V2::ApiController
   def index
-    render json: {
-      lodgings: GetAutocompleteData.call({ query: params[:query], type: 'lodgings' }, locale),
-      campaigns: GetAutocompleteData.call({ query: params[:query], type: 'campaigns' }, locale),
-      countries: GetAutocompleteData.call({ query: params[:query], type: 'countries' }, locale),
-      regions: GetAutocompleteData.call({ query: params[:query], type: 'regions' }, locale),
-    }
+    render json: [
+      {
+        title: 'Accommodations',
+        data: GetAutocompleteData.call({ query: params[:query], type: 'lodgings' }, locale),
+      },
+      {
+        title: 'Popular Collection',
+        data: GetAutocompleteData.call({ query: params[:query], type: 'campaigns' }, locale)
+      },
+      {
+        title: 'Countries',
+        data: GetAutocompleteData.call({ query: params[:query], type: 'countries' }, locale)
+      },
+      {
+        title: 'Regions',
+        data: GetAutocompleteData.call({ query: params[:query], type: 'regions' }, locale),
+      },
+    ]
   end
 end
