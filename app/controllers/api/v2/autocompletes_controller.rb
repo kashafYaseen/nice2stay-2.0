078f0d96394031d62d56480e93d1a0c3010aa070
Sikandar Tariq <sikandar.tariq@thedevden.co>
2019-11-28 15:59:49 +0500

    Change autocomplete data order for mobile api

--- app/controllers/api/v2/autocompletes_controller.rb
+++ app/controllers/api/v2/autocompletes_controller.rb
@@ -2,20 +2,20 @@ class Api::V2::AutocompletesController < Api::V2::ApiController
   def index
     render json: [
       {
-        title: 'Accommodations',
-        data: GetAutocompleteData.call({ query: params[:query], type: 'lodgings' }, locale),
-      },
-      {
-        title: 'Popular Collection',
-        data: GetAutocompleteData.call({ query: params[:query], type: 'campaigns' }, locale)
-      },
-      {
         title: 'Countries',
         data: GetAutocompleteData.call({ query: params[:query], type: 'countries' }, locale)
       },
       {
         title: 'Regions',
         data: GetAutocompleteData.call({ query: params[:query], type: 'regions' }, locale),
+      },
+      {
+        title: 'Popular Collection',
+        data: GetAutocompleteData.call({ query: params[:query], type: 'campaigns' }, locale)
+      },
+      {
+        title: 'Accommodations',
+        data: GetAutocompleteData.call({ query: params[:query], type: 'lodgings' }, locale),
       },
     ]
   end
