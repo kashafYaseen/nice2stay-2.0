843fff2330bcc5a59f87bfa5cf3288f4d644e9c4
Sikandar Tariq <sikandar.tariq@thedevden.co>
2019-12-17 17:07:47 +0500

    Add email and image-url to user serializer

--- app/serializers/api/v2/user_serializer.rb
+++ app/serializers/api/v2/user_serializer.rb
@@ -1,7 +1,7 @@
 class Api::V2::UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :full_name, :first_name, :last_name,
-             :created_at, :updated_at
+  attributes :id, :full_name, :first_name, :last_name, :email,
+             :created_at, :updated_at, :image_url, :address, :language
 
   attribute :auth_token, if: Proc.new { |user, params|
     params && params[:auth_token] == true
