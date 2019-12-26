9b69f5e328e017369cda073a3c4c8c8cdb267641
Sikandar Tariq <sikandar.tariq@thedevden.co>
2019-12-26 15:30:27 +0500

    Add countries in home page request for mobile api

--- app/controllers/api/v2/pages_controller.rb
+++ app/controllers/api/v2/pages_controller.rb
@@ -2,7 +2,8 @@ class Api::V2::PagesController < Api::V2::ApiController
   def home
     render json: {
       popular_lodgings: Api::V2::LodgingSerializer.new(Lodging.home_page).serializable_hash,
-      campaigns: Api::V2::CampaignSerializer.new(CustomText.home_page.popular.includes(:country, :region, :experience)).serializable_hash
+      campaigns: Api::V2::CampaignSerializer.new(CustomText.home_page.popular.includes(:country, :region, :experience)).serializable_hash,
+      locations: Api::V2::CountrySerializer(Country.enabled).serializable_hash,
     }, status: :ok
   end
 end
