f28526ae97d3b2e46ba4096c1bc121c7380bdfa1
Sikandar Tariq <sikandar.tariq@thedevden.co>
2020-01-07 16:53:33 +0500

    Update sync service to save amenity category details

--- app/services/save_amenity_details.rb
+++ app/services/save_amenity_details.rb
@@ -15,12 +15,14 @@ class SaveAmenityDetails
     save_amenity
     save_lodgings
     update_translations
+    update_category_translations(amenity.amenity_category)
     amenity
   end
 
   private
     def save_amenity
       amenity.attributes = amenity_params
+      amenity.amenity_category = amenity_category
       amenity.save
     end
 
@@ -38,6 +40,22 @@ class SaveAmenityDetails
       end
     end
 
+    def update_category_translations category
+      return unless params[:category_translations].present? && category.present?
+      params[:category_translations].each do |translation|
+        _translation = category.translations.find_or_initialize_by(locale: translation[:locale])
+        _translation.attributes = category_translation_params(translation)
+        _translation.save
+      end
+    end
+
+    def amenity_category
+      category = AmenityCategory.find_or_initialize_by(crm_id: category_params[:crm_id])
+      category.attributes = category_params
+      category.save
+      category
+    end
+
     def amenity_params
       params.require(:amenity).permit(
         :slug,
@@ -51,6 +69,20 @@ class SaveAmenityDetails
     def translation_params(translation)
       translation.permit(
         :slug,
+        :name,
+        :locale,
+      )
+    end
+
+    def category_params
+      params.require(:category).permit(
+        :name,
+        :crm_id,
+      )
+    end
+
+    def category_translation_params(translation)
+      translation.permit(
         :name,
         :locale,
       )
