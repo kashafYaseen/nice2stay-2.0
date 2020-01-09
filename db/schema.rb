4a94fbc21e0e1837ef8d23da9e41a45e55656a5b
Sikandar Tariq <sikandar.tariq@thedevden.co>
2020-01-09 16:07:37 +0500

    Add gc policy to reservations and update sync service

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_07_112112) do
+ActiveRecord::Schema.define(version: 2020_01_09_110629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -840,6 +840,7 @@ ActiveRecord::Schema.define(version: 2020_01_07_112112) do
     t.float "tax", default: 0.0
     t.float "additional_fee", default: 0.0
     t.string "room_type"
+    t.text "gc_policy"
     t.index ["booking_id"], name: "index_reservations_on_booking_id"
     t.index ["lodging_id"], name: "index_reservations_on_lodging_id"
   end
