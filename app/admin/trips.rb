3bd9ff507daadb13d839dae96722d1e4d82d67fc
Sikandar Tariq <sikandar.tariq@thedevden.co>
2020-01-20 17:15:06 +0500

    Add trips admin controller

---
+++ app/admin/trips.rb
@@ -0,0 +1,49 @@
+ActiveAdmin.register Trip do
+  actions :index, :show
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :user do |trip|
+      trip.trip_members.first.try(:user)
+    end
+
+    column :accommodations do |trip|
+      trip.wishlists.count
+    end
+
+    column :visibility
+    column :created_at
+
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :name
+      row :check_in
+      row :check_out
+      row :visibility
+      row :adults
+      row :children
+      row :budget
+      row :created_at
+      row :updated_at
+    end
+
+    panel "Accommodations" do
+      table_for trip.wishlists do
+        column :lodging
+      end
+    end
+
+    panel "Members" do
+      table_for trip.trip_members do
+        column :user
+      end
+    end
+
+    active_admin_comments
+  end
+end
