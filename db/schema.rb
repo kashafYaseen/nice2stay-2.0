d878104d6022b95c606b04eba5d614373197f4e3
Sikandar Tariq <sikandar.tariq@thedevden.co>
2020-03-20 16:11:15 +0500

    Add model newsletter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_113135) do
+ActiveRecord::Schema.define(version: 2020_03_20_111006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -626,6 +626,13 @@ ActiveRecord::Schema.define(version: 2020_03_19_113135) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+  end
+
+  create_table "newsletter_subscriptions", force: :cascade do |t|
+    t.string "email"
+    t.string "language"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "notifications", force: :cascade do |t|
