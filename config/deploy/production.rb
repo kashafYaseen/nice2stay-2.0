60aad32dd7f6a440c25844ef48bbef0aefd7e79a
Ahmad Saleem <ahmadsaleem93@gmail.com>
2020-06-03 04:28:44 +0500

    Increase number of puma workers.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,8 +5,8 @@ server '149.210.229.119', port: 22, roles: [:web, :app, :db], primary: true
 set :repo_url,        'git@github.com:remcoz/geolocation.git'
 set :application,     'geolocation'
 set :user,            'deploy'
-set :puma_threads,    [4, 16]
-set :puma_workers,    0
+set :puma_threads,    [2, 8]
+set :puma_workers,    6
 
 # Don't change these unless you know what you're doing
 set :pty,             true
