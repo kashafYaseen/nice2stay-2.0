020476b7299329c9b680d2f5c4604e1ed423717b
Ahmad Saleem <ahmadsaleem93@gmail.com>
2020-08-18 13:12:13 +0500

    Add a couple of missing attributes.

--- app/serializers/api/v2/lodging_serializer.rb
+++ app/serializers/api/v2/lodging_serializer.rb
@@ -1,16 +1,12 @@
 class Api::V2::LodgingSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name, :h1, :h2, :lodging_type, :presentation, :child_name,
-             :address, :latitude, :longitude, :adults, :children, :infants,
+  attributes :id, :name, :h1, :h2, :lodging_type, :presentation, :child_name, :address,
+             :latitude, :longitude, :adults, :children, :infants, :country_name, :region_name,
              :price, :calculated_price, :dynamic_price, :summary, :description, :short_desc,
              :images, :thumbnails, :average_rating, :created_at, :updated_at
 
   attribute :total_reviews do |lodging|
     lodging.all_reviews.count
-  end
-
-  attribute :country_name do |lodging|
-    lodging.country.try(:name) if lodging.home_page?
   end
 
   attributes :amenities, if: Proc.new { |lodging, params| params.present? && params[:amenities].present? } do |lodging, params|
