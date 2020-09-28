61cdfce3538e806b55c7126e37261eff91055571
fuadkhan1996 <fuad.khan@thedevden.co>
2020-09-28 18:08:02 +0500

    Added soap envelope and body to availabilities api

--- app/controllers/api/v1/room_raccoon/availabilities_controller.rb
+++ app/controllers/api/v1/room_raccoon/availabilities_controller.rb
@@ -1,8 +1,11 @@
 class Api::V1::RoomRaccoon::AvailabilitiesController < Api::V1::RoomRaccoon::ApiController
   def create
     @body = Hash.from_xml request.body.read
+    @body = @body['Envelope']['Body']
     hotel_id = @body['OTA_HotelAvailNotifRQ']['AvailStatusMessages']['HotelCode']
-    hotel = Lodging.find(hotel_id)
+    hotel = Lodging.find_by(id: hotel_id)
+    byebug
+
     response = RoomRaccoons::CreateAvailabilities.call(@body, hotel)
     if response
       render xml: API::V1::RoomRaccoon::CreateAvailabilitiesResponse.new(@body).success, status: :ok
