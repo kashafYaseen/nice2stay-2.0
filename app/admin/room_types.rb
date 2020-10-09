7a115db48f3ea13e8f753e2a2402871331feecb4
fuadkhan1996 <fuad.khan@thedevden.co>
2020-10-09 16:41:31 +0500

    Added rooms to room type show page in active admin

--- app/admin/room_types.rb
+++ app/admin/room_types.rb
@@ -15,4 +15,33 @@ ActiveAdmin.register RoomType do
 
     actions
   end
+
+
+  show do
+    attributes_table do
+      row :code
+      row :description
+      row :parent_lodging
+    end
+
+    panel "Rooms" do
+      table_for room_type.child_lodgings do
+        column :owner
+        column :title
+        column :subtitle
+        column :street
+        column :city
+        column :zip
+        column :state
+        column :lodging_type
+        column :description
+
+        column 'Actions' do |lodging|
+          link_to 'View', admin_lodging_path(lodging)
+        end
+      end
+    end
+
+    active_admin_comments
+  end
 end
