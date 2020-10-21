d2276b74bea929635434d7d5c5000f5886ccb9c8
adnanashraf107 <adnan.ashraf@thedevden.co>
2020-10-21 18:49:10 +0500

    feat: add lodging count in region serializer

--- app/serializers/api/v2/region_serializer.rb
+++ app/serializers/api/v2/region_serializer.rb
@@ -1,4 +1,8 @@
 class Api::V2::RegionSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :name, :slug, :title, :content, :image, :country_id
+
+  attribute :lodging_count do |region|
+    region.lodgings_published_parents_count
+  end
 end
