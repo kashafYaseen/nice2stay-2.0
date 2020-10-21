3bf12534f697d234541a0d7ec21b54301cacdb76
adnanashraf107 <adnan.ashraf@thedevden.co>
2020-10-21 16:29:12 +0500

    feat: filter regions based on country slug

--- app/controllers/api/v2/regions_controller.rb
+++ app/controllers/api/v2/regions_controller.rb
@@ -1,6 +1,7 @@
 class Api::V2::RegionsController < Api::V2::ApiController
   def index
     params[:search][:country_id_in] = JSON.parse(params[:search][:country_id_in]) if params[:search].present? && params[:search][:country_id_in].present?
+    params[:search][:country_id_in] = [Country.friendly.find(params[:search][:country_slug]).id] if params[:search].present? && params[:search][:country_slug].present?
     regions = Region.ransack(params[:search]).result
     render json: Api::V2::RegionSerializer.new(regions).serialized_json, status: :ok
   end
