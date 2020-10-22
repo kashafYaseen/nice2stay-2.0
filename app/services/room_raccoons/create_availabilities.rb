8276a51a9bea553525b85ddd96bb5dc7c8e73e09
fuadkhan1996 <fuad.khan@thedevden.co>
2020-10-22 18:07:31 +0500

    Refactored create_availabilities request room raccoon

--- app/services/room_raccoons/create_availabilities.rb
+++ app/services/room_raccoons/create_availabilities.rb
@@ -13,30 +13,12 @@ class RoomRaccoons::CreateAvailabilities
 
   def call
     begin
-      @body['ota_hotelavailnotifrq']['availstatusmessages']['availstatusmessage'].each do |avail_status_message|
-        start_date, end_date, room_type_code, rate_plan_code, status, restriction, stays = parse_data(avail_status_message)
-
-        dates = (start_date..end_date).map(&:to_s)
-        stays = stays.length == 2 ? (stays[0]..stays[1]).map(&:to_s) : stays
-
-        if rate_plan_code.present?
-          @rooms = hotel.room_types.find_by(code: room_type_code).child_lodgings.by_rate_code(rate_plan_code.upcase)
-        else
-          @rooms = hotel.room_types.find_by(code: room_type_code).child_lodgings
+      if @body['availstatusmessages']['availstatusmessage'].is_a?(Array)
+        @body['availstatusmessages']['availstatusmessage'].each do |avail_status_message|
+          create_availabilities avail_status_message
         end
-
-        @rooms.each do |room|
-          dates.each do |date|
-            room.availabilities.find_or_create_by(available_on: date)
-
-            check_response = restriction_status(status, restriction)
-            if check_response.present?
-              rule = room.rules.find_or_initialize_by(start_date: start_date, end_date: end_date) { |rule| rule.minimum_stay = stays }
-              check_response == "check_in_closed" ? rule.rr_check_in_closed = true : rule.rr_check_out_closed = true
-              rule.save
-            end
-          end
-        end
+      else
+        create_availabilities @body['availstatusmessages']['availstatusmessage']
       end
       return true
     rescue
@@ -45,7 +27,7 @@ class RoomRaccoons::CreateAvailabilities
   end
 
   private
-    def parse_data(data)
+    def parse_data data
       status_application_control = data['statusapplicationcontrol']
       if status_application_control.present?
         @start = status_application_control["start"]&.to_date
@@ -63,8 +45,12 @@ class RoomRaccoons::CreateAvailabilities
       length_of_stays = data['lengthsofstay']
       @stays = []
       if length_of_stays.present?
-        length_of_stays["lengthofstay"].each do |length_of_stay|
-          @stays << length_of_stay["time"]
+        if length_of_stays['lengthofstay'].is_a?(Array)
+          length_of_stays["lengthofstay"].each do |length_of_stay|
+            @stays << length_of_stay["time"]
+          end
+        else
+          @stays << length_of_stays['lengthofstay']["time"]
         end
       end
 
@@ -74,5 +60,52 @@ class RoomRaccoons::CreateAvailabilities
     def restriction_status(status, restriction)
       return "check_out_closed" if status == "close" && restriction == "departure"
       return "check_in_closed" if status == "close" && restriction == "arrival"
+    end
+
+    def create_availabilities avail_status_message
+      start_date, end_date, room_type_code, rate_plan_code, status, restriction, stays = parse_data(avail_status_message)
+
+      dates = (start_date..end_date).map(&:to_s)
+      stays = stays.length == 2 ? (stays[0]..stays[1]).map(&:to_s) : stays
+
+      if rate_plan_code.present?
+        @rooms = hotel.room_types.find_by(code: room_type_code).child_lodgings.by_rate_code(rate_plan_code.upcase).includes(:availabilities, :rules)
+      else
+        @rooms = hotel.room_types.find_by(code: room_type_code).child_lodgings.includes(:availabilities, :rules)
+      end
+
+      availabilities = []
+      rules = []
+      @rooms.each do |room|
+        available_on_dates = room.availabilities.pluck(:available_on)
+
+        dates.each do |date|
+          unless available_on_dates.include?(date.to_date)
+            availabilities << room.availabilities.new(available_on: date, created_at: DateTime.now, updated_at: DateTime.now)
+          end
+        end
+
+        check_response = restriction_status(status, restriction)
+        if check_response.present?
+          rule_index = room.rules.find_index {|rule| rule.start_date == start_date.to_date && rule.end_date == end_date.to_date }
+          if rule_index.present?
+            rule = room.rules[rule_index]
+            check_response == "check_in_closed" ? rule.rr_check_in_closed = true : rule.rr_check_out_closed = true
+            rule.save
+          else
+            rule = room.rules.new(start_date: start_date, end_date: end_date, minimum_stay: stays, created_at: DateTime.now, updated_at: DateTime.now)
+            check_response == "check_in_closed" ? rule.rr_check_in_closed = true : rule.rr_check_out_closed = true
+            rules << rule
+          end
+        end
+      end
+
+      if availabilities.present?
+        Availability.import availabilities, batch_size: 150
+      end
+
+      if rules.present?
+        Rule.import rules, batch: 150
+      end
     end
 end
