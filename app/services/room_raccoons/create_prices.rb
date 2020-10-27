63e8bd03861f29a5f15a33028b32ec475a88e0f8
fuadkhan1996 <fuad.khan@thedevden.co>
2020-10-27 12:12:09 +0500

    Refactored and modified prices api service for room raccoon

--- app/services/room_raccoons/create_prices.rb
+++ app/services/room_raccoons/create_prices.rb
@@ -38,29 +38,23 @@ class RoomRaccoons::CreatePrices
       rates = []
       if base_by_guest_amts.kind_of?(Array)
         base_by_guest_amts.each do |base_by_guest_amt|
-          rates << {
-            age_qualifying_code: base_by_guest_amt['agequalifyingcode'],
-            guests: base_by_guest_amt['numberofguests'],
-            amount: base_by_guest_amt['amountaftertax']
-          }
+          rates << base_by_guests_hash(base_by_guest_amt)
         end
       else
-        rates << {
-          age_qualifying_code: base_by_guest_amts['agequalifyingcode'],
-          guests: base_by_guest_amts['numberofguests'],
-          amount: base_by_guest_amts['amountaftertax']
-        }
+        rates << base_by_guests_hash(base_by_guest_amts)
       end
 
       additional_guest_amounts = data['rates']['rate']['additionalguestamounts']
       if additional_guest_amounts.present?
         additional_amounts = []
         additional_guest_amounts = data['rates']['rate']['additionalguestamounts']['additionalguestamount']
-        additional_guest_amounts.each do |additional_guest_amount|
-          additional_amounts << {
-            age_qualifying_code: additional_guest_amount['agequalifyingcode'],
-            amount: additional_guest_amount['amount']
-          }
+
+        if additional_guest_amounts.kind_of?(Array)
+          additional_guest_amounts.each do |additional_guest_amount|
+            additional_amounts << additional_guests_hash(additional_guest_amount)
+          end
+        else
+          additional_amounts << additional_guests_hash(additional_guest_amounts)
         end
       end
 
@@ -74,39 +68,103 @@ class RoomRaccoons::CreatePrices
       end_date,
       rates,
       additional_amounts = parse_data(rate_amount_message)
-      rooms = hotel.room_types.find_by(code: room_type_code)&.child_lodgings.includes(:availabilities, :cleaning_costs).where('availabilities.available_on >= ? and availabilities.available_on <= ?', start_date, end_date).references(:availabilities)
 
-      prices = []
-      cleaning_costs = []
-      rooms&.each do |room|
+      @rooms = hotel.room_types.find_by(code: room_type_code)&.child_lodgings.includes(:cleaning_costs, availabilities: :prices).where('availabilities.available_on >= ? and availabilities.available_on <= ?', start_date, end_date).references(:availabilities)
+
+      new_prices = []
+      new_cleaning_costs = []
+
+      @rooms.each do |room|
         room.availabilities.each do |availability|
+          prices = availability.prices
+
           rates.each do |rate|
-            price = availability.prices.new(amount: rate[:amount], rr_rate_plan_code: rate_plan_code, created_at: DateTime.now, updated_at: DateTime.now)
-            (rate[:age_qualifying_code].present? && rate[:age_qualifying_code] == "8") ? price.children = [rate[:guests]] : price.adults = [rate[:guests]]
-            price.rr_rate_plan_code = rate_plan_code
-            # price.save
-            prices << price
+            if rate[:age_qualifying_code].present? && rate[:guests].present?
+              @price_index = prices.find_index {|price|
+                (rate[:age_qualifying_code] == "7" && price.infants == [rate[:guests]]) ||
+                (rate[:age_qualifying_code] == "8" && price.children == [rate[:guests]]) ||
+                (rate[:age_qualifying_code] == "10" && price.adults == [rate[:guests]])
+              }
+            else
+              @price_index = prices.find_index {|price| !(price.children.present? || price.adults.present? || price.infants.present?) }
+            end
+
+            if @price_index.present?
+              @price = prices[@price_index]
+              @price.amount = rate[:amount]
+            else
+              @price = availability.prices.new(amount: rate[:amount], rr_rate_plan_code: rate_plan_code, created_at: DateTime.now, updated_at: DateTime.now)
+            end
+
+            if rate[:guests].present?
+              if rate[:age_qualifying_code] == "7"
+                @price.infants = [rate[:guests]]
+              elsif rate[:age_qualifying_code] == "8"
+                @price.children = [rate[:guests]]
+              else
+                @price.adults = [rate[:guests]]
+              end
+            end
+
+            if @price.new_record?
+              new_prices << @price
+            elsif @price.changed?
+              @price.save
+            end
           end
         end
 
         if additional_amounts.present?
+          cleaning_costs = room.cleaning_costs
+
           additional_amounts.each do |additional_amount|
-            cleaning_costs << room.cleaning_costs.new(
-              fixed_price: additional_amount[:amount],
-              name: additional_amount[:age_qualifying_code] == "10" ? "Adults" : "Children",
-              created_at: DateTime.now,
-              updated_at: DateTime.now
-            )
+            cleaning_cost_index = cleaning_costs.find_index {|cleaning_cost|
+              (additional_amount[:age_qualifying_code] == "7" && cleaning_cost.name == "Infants") ||
+              (additional_amount[:age_qualifying_code] == "8" && cleaning_cost.name == "Children") ||
+              (additional_amount[:age_qualifying_code] == "10" && cleaning_cost.name == "Adults")
+            }
+
+            if cleaning_cost_index.present?
+              cleaning_cost = cleaning_costs[cleaning_cost_index]
+              cleaning_cost.amount = additional_amount[:amount]
+              cleaning_cost.save
+            else
+              cleaning_cost = cleaning_costs.new( fixed_price: additional_amount[:amount], created_at: DateTime.now, updated_at: DateTime.now)
+              if additional_amount[:age_qualifying_code] == "7"
+                cleaning_cost.name = "Infants"
+              elsif additional_amount[:age_qualifying_code] == "8"
+                cleaning_cost.name = "Children"
+              else
+                cleaning_cost.name = "Adults"
+              end
+
+              new_cleaning_costs << cleaning_cost
+            end
           end
         end
       end
 
-      if prices.present?
-        Price.import prices, batch_size: 150
+      if new_prices.present?
+        Price.import new_prices, batch_size: 150
       end
 
-      if cleaning_costs.present?
-        CleaningCost.import cleaning_costs, batch: 150
+      if new_cleaning_costs.present?
+        CleaningCost.import new_cleaning_costs, batch: 150
       end
+    end
+
+    def base_by_guests_hash base_by_guest_amt
+      {
+        age_qualifying_code: base_by_guest_amt['agequalifyingcode'],
+        guests: base_by_guest_amt['numberofguests'],
+        amount: base_by_guest_amt['amountaftertax']
+      }
+    end
+
+    def additional_guests_hash additional_guest_amount
+      {
+        age_qualifying_code: additional_guest_amount['agequalifyingcode'],
+        amount: additional_guest_amount['amount']
+      }
     end
 end
