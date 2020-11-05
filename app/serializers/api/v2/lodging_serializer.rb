e24fbde441b9a16b0db8acf438e6fea9a370c31b
adnanashraf107 <adnan.ashraf@thedevden.co>
2020-11-05 09:59:06 +0500

    feat: add channel in lodging apis

--- app/serializers/api/v2/lodging_serializer.rb
+++ app/serializers/api/v2/lodging_serializer.rb
@@ -4,7 +4,7 @@ class Api::V2::LodgingSerializer
             :region_name, :address, :latitude, :longitude, :adults, :children, :infants,
              :price, :calculated_price, :dynamic_price, :summary, :description, :short_desc,
              :images, :thumbnails, :average_rating, :created_at, :updated_at, :highlight_1,
-             :highlight_2, :highlight_3, :beds, :baths
+             :highlight_2, :highlight_3, :beds, :baths, :channel
 
   attribute :total_reviews do |lodging|
     lodging.all_reviews.count
