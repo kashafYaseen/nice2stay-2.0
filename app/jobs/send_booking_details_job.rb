9cef7300f338745028b0e1572aec934d09b5f48f
fuadkhan1996 <fuad.khan@thedevden.co>
2020-11-11 13:26:51 +0500

    Changed send_bookings_details_job queue to default

--- app/jobs/send_booking_details_job.rb
+++ app/jobs/send_booking_details_job.rb
@@ -1,5 +1,5 @@
 class SendBookingDetailsJob < ApplicationJob
-  queue_as :rr_prices_queue
+  queue_as :default
 
   def perform(booking_id)
     SendBookingDetails.call(Booking.find_by(id: booking_id))
