01aaa7700eb6baacd19f8e83d0826b6cb77522b5
fuadkhan1996 <fuad.khan@thedevden.co>
2020-11-11 13:24:44 +0500

    Some refactoring and added sidekiq adapter

--- app/jobs/send_booking_details_job.rb
+++ app/jobs/send_booking_details_job.rb
@@ -1,5 +1,5 @@
 class SendBookingDetailsJob < ApplicationJob
-  queue_as :default
+  queue_as :rr_prices_queue
 
   def perform(booking_id)
     SendBookingDetails.call(Booking.find_by(id: booking_id))
