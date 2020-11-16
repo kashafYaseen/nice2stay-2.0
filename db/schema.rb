9c2d11a8294acdb920d6c5ca96e6cb34637545ad
fuadkhan1996 <fuad.khan@thedevden.co>
2020-11-16 17:27:39 +0500

    Added room_type reference to availabilities in db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_094147) do
+ActiveRecord::Schema.define(version: 2020_11_16_122502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -166,9 +166,11 @@ ActiveRecord::Schema.define(version: 2020_11_16_094147) do
     t.boolean "check_out_only", default: false
     t.bigint "lodging_id"
     t.bigint "rate_plan_id"
+    t.bigint "room_type_id"
     t.index ["lodging_id", "available_on"], name: "index_availabilities_on_lodging_id_and_available_on", unique: true
     t.index ["lodging_id"], name: "index_availabilities_on_lodging_id"
     t.index ["rate_plan_id"], name: "index_availabilities_on_rate_plan_id"
+    t.index ["room_type_id"], name: "index_availabilities_on_room_type_id"
   end
 
   create_table "bookings", force: :cascade do |t|
