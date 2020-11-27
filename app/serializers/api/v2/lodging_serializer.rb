89a3bb434305bc565ba7a79294dda7ffc837f257
adnanashraf107 <adnan.ashraf@thedevden.co>
2020-11-27 12:16:00 +0500

    fix: add including text in lodging serializer

--- app/serializers/api/v2/lodging_serializer.rb
+++ app/serializers/api/v2/lodging_serializer.rb
@@ -2,7 +2,7 @@ class Api::V2::LodgingSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :name, :h1, :h2, :lodging_type, :slug, :presentation, :child_name, :country_name,
             :region_name, :address, :latitude, :longitude, :adults, :children, :infants,
-             :price, :calculated_price, :dynamic_price, :summary, :description, :short_desc,
+             :price, :calculated_price, :dynamic_price, :summary, :description, :short_desc, :including_text,
              :images, :thumbnails, :average_rating, :created_at, :updated_at, :highlight_1,
              :highlight_2, :highlight_3, :beds, :baths, :channel
 
