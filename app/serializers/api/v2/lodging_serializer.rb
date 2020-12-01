07a055d1b23ebc80df00370d2ccea0e4e83ec6a7
adnanashraf107 <adnan.ashraf@thedevden.co>
2020-12-01 14:39:15 +0500

    feat: update api response

--- app/serializers/api/v2/lodging_serializer.rb
+++ app/serializers/api/v2/lodging_serializer.rb
@@ -4,7 +4,7 @@ class Api::V2::LodgingSerializer
             :region_name, :address, :latitude, :longitude, :adults, :children, :infants,
              :price, :calculated_price, :dynamic_price, :summary, :description, :short_desc, :including_text,
              :images, :thumbnails, :average_rating, :created_at, :updated_at, :highlight_1,
-             :highlight_2, :highlight_3, :beds, :baths, :channel
+             :highlight_2, :highlight_3, :beds, :baths, :channel, :particularities_text
 
   attribute :total_reviews do |lodging|
     lodging.all_reviews.count
