58a4038fb2760dc170fa274ffc0a0e19122b35c8
adnanashraf107 <adnan.ashraf@thedevden.co>
2020-12-18 15:21:36 +0500

    feat: update room type serializer response

--- app/serializers/api/v2/room_type_serializer.rb
+++ app/serializers/api/v2/room_type_serializer.rb
@@ -1,7 +1,6 @@
 class Api::V2::RoomTypeSerializer
   include FastJsonapi::ObjectSerializer
-
-  attributes :id, :code, :description
+  attributes :id, :code, :description, :adults, :children, :infants
 
   attributes :rate_plans do |room_type|
     Api::V2::RatePlanSerializer.new(room_type.rate_plans)
