668b2857a7328c86465ca4fd546ab007bcd30c3d
fuadkhan1996 <fuad.khan@thedevden.co>
2021-01-26 10:56:20 +0500

    Bug fix in create_rates service opengds

--- app/services/open_gds/create_rates.rb
+++ app/services/open_gds/create_rates.rb
@@ -124,7 +124,8 @@ class OpenGds::CreateRates
 
       availabilities, prices = update_availabilities rate_params, room_rate, rate_plan
       availabilities = room_rate.availabilities if availabilities.blank?
-      update_availabilities_by_status rate_plan, accom_params, room_rate, availabilities, prices.flatten
+      prices = prices.flatten
+      update_availabilities_by_status rate_plan, accom_params, room_rate, availabilities, prices
       { room_rate: room_rate, availabilities: availabilities, prices: prices }
     end
 
@@ -218,11 +219,8 @@ class OpenGds::CreateRates
 
     def set_price(params, availability)
       price = availability.prices.find { |p| p[:adults] == ['999'] && p[:children] == ['0'] && p[:infants] == ['0'] }.presence || availability.prices.new(adults: ['999'], children: ['0'], infants: ['0'])
-      stays = []
-      stays << params[:minlos] if params[:minlos].present?
-      stays << params[:maxlos] if params[:maxlos].present?
       price.amount = params[:rate] if params[:rate].present?
-      price.minimum_stay = (stays[0]..stays[-1]).map(&:to_s) if stays.present?
+      price.minimum_stay = availability.rr_minimum_stay
       price.open_gds_single_rate = params[:single_rate] if params[:single_rate].present?
       price.multiple_checkin_days = params[:checkin_days] if params[:checkin_days].present?
       if price.new_record?
