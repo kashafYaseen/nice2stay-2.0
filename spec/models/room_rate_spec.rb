5a0b9312f096dcf691366783c2f545dc367da22b
fuadkhan1996 <fuad.khan@thedevden.co>
2021-02-10 12:34:12 +0500

    Text change for test cases

--- spec/models/room_rate_spec.rb
+++ spec/models/room_rate_spec.rb
@@ -14,14 +14,14 @@ RSpec.describe RoomRate, type: :model do
       }
     }
 
-    it 'should return price for only 1 adult for rate plan with Per Stay Rate Type and Single Supplement Single Rate Type' do
+    it 'should return price for only 1 adult for rate plan with Rate Type = \'Per Stay\' and Single Rate Type = \'Single Supplement\'' do
       rate_plan = create(:rate_plan_with_child_rates_and_rule, open_gds_rate_type: 3, open_gds_single_rate_type: 0, min_stay: 3, max_stay: 3, categories: [1, 2], rates: [10, 20], lodging_id: lodging.id)
       room_rate = create(:room_rate_with_availabilities, room_type: room_type, rate_plan: rate_plan, min_stay: ['3'])
       room_rate.cumulative_price(params)
       expect(room_rate.calculated_price).to eq(252)
     end
 
-    it 'should return price for only 1 adult for rate plan with Per Stay Rate Type and Fixed Rate Single Rate Type' do
+    it 'should return price for only 1 adult for rate plan with Rate Type = \'Per Stay\' and Single Rate Type = \'Single Rate\'' do
       rate_plan = create(:rate_plan_with_child_rates_and_rule, open_gds_rate_type: 3, open_gds_single_rate_type: 1, min_stay: 3, max_stay: 3, categories: [1, 2], rates: [10, 20], lodging_id: lodging.id)
       room_rate = create(:room_rate_with_availabilities, room_type: room_type, rate_plan: rate_plan, min_stay: ['3'])
       room_rate.cumulative_price(params)
