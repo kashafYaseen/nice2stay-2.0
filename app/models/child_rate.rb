4831214b56cef4904f1659feba86116636001780
fuadkhan1996 <fuad.khan@thedevden.co>
2021-02-10 10:23:53 +0500

    Removed infants and children scopes from childrate model

--- app/models/child_rate.rb
+++ app/models/child_rate.rb
@@ -1,9 +1,6 @@
 class ChildRate < ApplicationRecord
   belongs_to :rate_plan
 
-  scope :infants, -> { where(age_group: ['Infants (0-1 year)', 'Toddlers (1-3 years)']) }
-  scope :children, -> { where.not(age_group: ['Infants (0-1 year)', 'Toddlers (1-3 years)']) }
-
   enum rate_type: {
     fixed_rate: 0,
     percentage: 1
