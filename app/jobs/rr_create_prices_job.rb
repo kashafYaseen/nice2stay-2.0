cf9aaccc49dc0ada70562ce31ddabbd37ee9f930
fuadkhan1996 <fuad.khan@thedevden.co>
2021-02-12 12:30:32 +0500

    Implement locking machanism for jobs to run synchronously

--- app/jobs/rr_create_prices_job.rb
+++ app/jobs/rr_create_prices_job.rb
@@ -2,11 +2,13 @@ class RrCreatePricesJob < ApplicationJob
   queue_as :rr_prices_queue
 
   def perform(hotel_id:, room_type_codes:, rate_plan_codes:, rr_prices:)
-    RoomRaccoons::CreatePrices.call(
-      hotel_id: hotel_id,
-      room_type_codes: room_type_codes,
-      rate_plan_codes: rate_plan_codes,
-      rr_prices: rr_prices
-    )
+    PgLock.new(name: 'room_raccoon', attempts: 10, attempt_interval: 60).lock do
+      RoomRaccoons::CreatePrices.call(
+        hotel_id: hotel_id,
+        room_type_codes: room_type_codes,
+        rate_plan_codes: rate_plan_codes,
+        rr_prices: rr_prices
+      )
+    end
   end
 end
