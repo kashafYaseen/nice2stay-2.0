b76b51404bc783dfc3067fc6ffa937329eb571f2
Fuad Khan <fuad.khan@thedevden.co>
2021-03-05 10:37:53 +0500

    Replace lodging_id with parent_lodging_id while creating rate plans in cummulative price test cases

--- spec/models/room_rate_spec.rb
+++ spec/models/room_rate_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe RoomRate, type: :model do
       let(:child_lodging) { create(:lodging, :reindex, channel: 3, name: 'Fabulous Room', description: 'Fabulous Room', parent_id: parent_lodging.id, region_id: region.id) }
 
       context 'When Rate Type = \'Per Stay\'' do
-        let(:rate_plan) { create(:rate_plan_with_child_rates_and_rule, open_gds_rate_type: 3, open_gds_single_rate_type: 0, min_stay: 3, max_stay: 3, categories: [1, 2], rates: [10, 20], lodging_id: parent_lodging.id) }
+        let(:rate_plan) { create(:rate_plan_with_child_rates_and_rule, open_gds_rate_type: 3, open_gds_single_rate_type: 0, min_stay: 3, max_stay: 3, categories: [1, 2], rates: [10, 20], parent_lodging_id: parent_lodging.id) }
 
         include_examples 'Calculate Prices For Opengds' do
           let(:room_rate) { create(:room_rate_with_availabilities, child_lodging: child_lodging, rate_plan: rate_plan, min_stay: ['3']) }
@@ -17,7 +17,7 @@ RSpec.describe RoomRate, type: :model do
       end
 
       context 'When Rate Type = \'Per Person\'' do
-        let(:rate_plan) { create(:rate_plan_with_child_rates_and_rule, open_gds_rate_type: 2, open_gds_single_rate_type: 0, min_stay: 3, max_stay: 3, categories: [1, 2], rates: [10, 20], lodging_id: parent_lodging.id) }
+        let(:rate_plan) { create(:rate_plan_with_child_rates_and_rule, open_gds_rate_type: 2, open_gds_single_rate_type: 0, min_stay: 3, max_stay: 3, categories: [1, 2], rates: [10, 20], parent_lodging_id: parent_lodging.id) }
 
         include_examples 'Calculate Prices For Opengds' do
           let(:room_rate) { create(:room_rate_with_availabilities, child_lodging: child_lodging, rate_plan: rate_plan, min_stay: ['3']) }
@@ -26,7 +26,7 @@ RSpec.describe RoomRate, type: :model do
       end
 
       context "When Rate Type = \'Per Person Per Night\'" do
-        let(:rate_plan) { create(:rate_plan_with_child_rates_and_rule, open_gds_rate_type: 0, open_gds_single_rate_type: 0, min_stay: 1, max_stay: 3, categories: [1, 2], rates: [10, 20], lodging_id: parent_lodging.id) }
+        let(:rate_plan) { create(:rate_plan_with_child_rates_and_rule, open_gds_rate_type: 0, open_gds_single_rate_type: 0, min_stay: 1, max_stay: 3, categories: [1, 2], rates: [10, 20], parent_lodging_id: parent_lodging.id) }
 
         include_examples 'Calculate Prices For Opengds' do
           let(:room_rate) { create(:room_rate_with_availabilities, child_lodging: child_lodging, rate_plan: rate_plan, min_stay: %w[1 2 3]) }
@@ -35,7 +35,7 @@ RSpec.describe RoomRate, type: :model do
       end
 
       context "When Rate Type = \'Per Person Per Day\'" do
-        let(:rate_plan) { create(:rate_plan_with_child_rates_and_rule, open_gds_rate_type: 4, open_gds_single_rate_type: 0, min_stay: 1, max_stay: 3, categories: [1, 2], rates: [10, 20], lodging_id: parent_lodging.id) }
+        let(:rate_plan) { create(:rate_plan_with_child_rates_and_rule, open_gds_rate_type: 4, open_gds_single_rate_type: 0, min_stay: 1, max_stay: 3, categories: [1, 2], rates: [10, 20], parent_lodging_id: parent_lodging.id) }
 
         include_examples 'Calculate Prices For Opengds' do
           let(:room_rate) { create(:room_rate_with_availabilities, child_lodging: child_lodging, rate_plan: rate_plan, min_stay: %w[1 2 3]) }
@@ -44,7 +44,7 @@ RSpec.describe RoomRate, type: :model do
       end
 
       context "When Rate Type = \'Per Accommodation Per Night\'" do
-        let(:rate_plan) { create(:rate_plan_with_child_rates_and_rule, open_gds_rate_type: 1, open_gds_single_rate_type: 0, min_stay: 1, max_stay: 3, categories: [1, 2], rates: [10, 20], lodging_id: parent_lodging.id) }
+        let(:rate_plan) { create(:rate_plan_with_child_rates_and_rule, open_gds_rate_type: 1, open_gds_single_rate_type: 0, min_stay: 1, max_stay: 3, categories: [1, 2], rates: [10, 20], parent_lodging_id: parent_lodging.id) }
 
         include_examples 'Calculate Prices For Opengds' do
           let(:room_rate) { create(:room_rate_with_availabilities, child_lodging: child_lodging, rate_plan: rate_plan, min_stay: %w[1 2 3]) }
@@ -53,7 +53,7 @@ RSpec.describe RoomRate, type: :model do
       end
 
       context "When Rate Type = \'Per Accommodation Per Day\'" do
-        let(:rate_plan) { create(:rate_plan_with_child_rates_and_rule, open_gds_rate_type: 5, open_gds_single_rate_type: 0, min_stay: 1, max_stay: 3, categories: [1, 2], rates: [10, 20], lodging_id: parent_lodging.id) }
+        let(:rate_plan) { create(:rate_plan_with_child_rates_and_rule, open_gds_rate_type: 5, open_gds_single_rate_type: 0, min_stay: 1, max_stay: 3, categories: [1, 2], rates: [10, 20], parent_lodging_id: parent_lodging.id) }
 
         include_examples 'Calculate Prices For Opengds' do
           let(:room_rate) { create(:room_rate_with_availabilities, child_lodging: child_lodging, rate_plan: rate_plan, min_stay: %w[1 2 3]) }
