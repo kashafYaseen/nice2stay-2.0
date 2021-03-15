0ae8b7c472671c45f57b02d38672b534dcbcb941
Fuad Khan <fuad.khan@thedevden.co>
2021-03-15 16:35:15 +0500

    Fix: change form_data to query_params and Post to Get for receipt_build opengds api

--- app/services/open_gds/receipt_build.rb
+++ app/services/open_gds/receipt_build.rb
@@ -8,13 +8,12 @@ class OpenGds::ReceiptBuild
 
   def initialize(reservation:)
     @reservation = reservation
-    @uri = URI.parse("https://api.opengds.com/core/v1/acc-status/receipt?#{credentials}")
+    @uri = URI.parse("https://api.opengds.com/core/v1/acc-status/receipt?#{query_params}")
   end
 
   def call
-    request = Net::HTTP::Post.new(uri.request_uri)
+    request = Net::HTTP::Get.new(uri.request_uri)
     request.content_type = 'application/x-www-form-urlencoded; charset=UTF-8'
-    request.set_form_data form_data
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     response = JSON.parse(http.request(request).body)
@@ -23,18 +22,8 @@ class OpenGds::ReceiptBuild
   end
 
   private
-    def form_data
-      {
-        rate_id: reservation.open_gds_rate_id,
-        accom_id: accommodation_ids,
-        arrival: reservation.check_in,
-        depart: reservation.check_out,
-        occupancy: occupancy,
-        first_name: reservation.user_first_name,
-        last_name: reservation.user_last_name,
-        phone: reservation.user_phone,
-        email: reservation.user_email
-      }
+    def query_params
+      "#{credentials}&rate_id=#{reservation.open_gds_rate_id}&accom_id=#{accommodation_ids}&arrival=#{reservation.check_in}&depart=#{reservation.check_out}&occupancy=#{occupancy}"
     end
 
     def accommodation_ids
