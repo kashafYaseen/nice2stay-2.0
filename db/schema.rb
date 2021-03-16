5b0e43256dad5806b4e3dc05679a6a7c6f0e7245
Fuad Khan <fuad.khan@thedevden.co>
2021-03-16 10:53:07 +0500

    Add publish field to room rates in db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_12_101712) do
+ActiveRecord::Schema.define(version: 2021_03_16_052158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1031,6 +1031,7 @@ ActiveRecord::Schema.define(version: 2021_03_12_101712) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "child_lodging_id"
+    t.boolean "publish", default: true
     t.index ["child_lodging_id"], name: "index_room_rates_on_child_lodging_id"
     t.index ["rate_plan_id"], name: "index_room_rates_on_rate_plan_id"
     t.index ["room_type_id"], name: "index_room_rates_on_room_type_id"
