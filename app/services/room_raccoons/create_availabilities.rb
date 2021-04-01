535add04d45b2bb89091084a6df4a7db52d2df2d
Fuad Khan <fuad.khan@thedevden.co>
2021-04-01 16:45:17 +0500

    Done changes in availabilities push API for room raccoon according to new schema changes

--- app/services/room_raccoons/create_availabilities.rb
+++ app/services/room_raccoons/create_availabilities.rb
@@ -1,37 +1,39 @@
 class RoomRaccoons::CreateAvailabilities
-  attr_reader :hotel_id, :room_type_codes, :rr_availabilities
+  attr_reader :hotel_id, :lodging_ids, :rr_availabilities
 
-  def self.call(hotel_id:, room_type_codes:, rr_availabilities:)
-    new(hotel_id: hotel_id, room_type_codes: room_type_codes, rr_availabilities: rr_availabilities).call
+  def self.call(hotel_id:, lodging_ids:, rr_availabilities:)
+    new(hotel_id: hotel_id, lodging_ids: lodging_ids, rr_availabilities: rr_availabilities).call
   end
 
-  def initialize(hotel_id:, room_type_codes:, rr_availabilities:)
+  def initialize(hotel_id:, lodging_ids:, rr_availabilities:)
     @hotel_id = hotel_id
-    @room_type_codes = room_type_codes
+    @lodging_ids = lodging_ids
     @rr_availabilities = rr_availabilities
   end
 
   def call
     availabilities = []
-    room_types = RoomType.includes(room_rates: [:rate_plan, availabilities: :prices]).where(parent_lodging_id: hotel_id, code: room_type_codes)
+    lodgings = Lodging.where(id: lodging_ids).includes(room_rates: [:rate_plan, availabilities: :prices])
     rr_availabilities.each do |data|
       dates = (data[:start_date].to_date..data[:end_date].to_date).map(&:to_s)
       stays = data[:stays].length == 2 ? (data[:stays][0]..data[:stays][1]).map(&:to_s) : data[:stays]
-      current_room_type = room_types.find { |room_type| room_type.code == data[:room_type_code] }
-      if data[:rate_plan_code].present?
-        @room_rates = current_room_type.room_rates.select { |room_rate| room_rate.rate_plan_code == data[:rate_plan_code] }
+      current_lodging = lodgings.find { |lodging| lodging.id == data[:lodging_id].to_i }
+
+      if data[:rate_plan_id].present?
+        @room_rates = current_lodging.room_rates.select { |room_rate| room_rate.rate_plan_id == data[:rate_plan_id].to_i }
       else
-        @room_rates = current_room_type.room_rates
+        @room_rates = current_lodging.room_rates
       end
 
       @room_rates.each do |current_room_rate|
         dates.each do |date|
           availability = current_room_rate.availabilities.find { |room_rate_availability| room_rate_availability.available_on.to_s == date }
           availability = current_room_rate.availabilities.new(available_on: date, room_rate: current_room_rate, created_at: DateTime.now, updated_at: DateTime.now) unless availability.present?
-          availability.rr_minimum_stay = stays if stays.present?
-          availability.rr_booking_limit = data[:booking_limit] if data[:booking_limit].present?
+          availability.rr_minimum_stay = stays.presence || ((current_room_rate.min_stay..current_room_rate.max_stay).map(&:to_s) if availability.rr_minimum_stay.blank?)
+          availability.rr_booking_limit = data[:booking_limit] || (current_room_rate.default_booking_limit if availability.rr_booking_limit.zero?)
           check_response = restriction_status(data[:status], data[:restriction])
-          if check_response.present? && check_response == 'booking_limit'
+
+          if check_response.present? && check_response == 'stop_sell'
             availability.rr_booking_limit = 0
           elsif check_response.present?
             check_response == 'check_in_closed' ? availability.rr_check_in_closed = true : availability.rr_check_out_closed = true
@@ -66,8 +68,9 @@ class RoomRaccoons::CreateAvailabilities
 
   private
     def restriction_status(status, restriction)
-      return unless status.present? && restriction.present?
-      return 'booking_limit' if status == 'close' && restriction.blank?
+      return unless status.present?
+
+      return 'stop_sell' if status == 'close' && restriction.blank?
       return 'check_out_closed' if status == 'close' && restriction == 'departure'
       return 'check_in_closed' if status == 'close' && restriction == 'arrival'
     end
