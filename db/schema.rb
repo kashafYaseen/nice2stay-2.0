3f073ff5d899c3fb4e27ce11df3f83490fe8bd92
Fuad Khan <fuad.khan@thedevden.co>
2021-04-09 17:55:55 +0500

    Add crm_id field to rate_plans in db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_02_095833) do
+ActiveRecord::Schema.define(version: 2021_04_09_125023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -877,6 +877,7 @@ ActiveRecord::Schema.define(version: 2021_04_02_095833) do
     t.integer "open_gds_single_rate_type"
     t.datetime "opengds_pushed_at"
     t.bigint "parent_lodging_id"
+    t.bigint "crm_id"
     t.index ["parent_lodging_id"], name: "index_rate_plans_on_parent_lodging_id"
   end
 
