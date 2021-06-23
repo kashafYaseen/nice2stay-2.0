8cf50d389705359e5e343c674711721d9d5577f5
maaqib121 <maaqib121@gmail.com>
2021-06-23 17:20:19 +0500

    fix: thumbnails spelling mistake

--- app/serializers/api/v2/lodging_serializer.rb
+++ app/serializers/api/v2/lodging_serializer.rb
@@ -3,7 +3,7 @@ class Api::V2::LodgingSerializer
   attributes :id, :name, :h1, :h2, :lodging_type, :slug, :presentation, :child_name, :country_name,
              :region_name, :address, :latitude, :longitude, :adults, :children, :infants,
              :price, :calculated_price, :dynamic_price, :summary, :description, :short_desc, :including_text,
-             :thumbails, :images, :average_rating, :created_at, :updated_at, :highlight_1,
+             :thumbnails, :images, :average_rating, :created_at, :updated_at, :highlight_1,
              :highlight_2, :highlight_3, :beds, :baths, :channel, :particularities_text,
              :open_gds_property_id, :open_gds_accommodation_id
 
