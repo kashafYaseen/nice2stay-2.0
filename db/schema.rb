6e7f3af1b9e985a8f2fb393ec93bd8d5bf3f0a52
Fuad Khan <fuad.khan@thedevden.co>
2021-09-07 16:53:36 +0500

    Add search_analytics db table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_30_105759) do
+ActiveRecord::Schema.define(version: 2021_09_06_102327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1070,6 +1070,12 @@ ActiveRecord::Schema.define(version: 2021_08_30_105759) do
     t.bigint "rate_plan_id"
     t.index ["lodging_id"], name: "index_rules_on_lodging_id"
     t.index ["rate_plan_id"], name: "index_rules_on_rate_plan_id"
+  end
+
+  create_table "search_analytics", force: :cascade do |t|
+    t.text "params"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "social_logins", force: :cascade do |t|
