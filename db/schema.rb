c3872dbcb11c542e48c612cae7d056a7fafe1c22
Fuad Khan <fuad.khan@thedevden.co>
2021-09-09 16:53:52 +0500

    Add crm_id in experiences in db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_09_105025) do
+ActiveRecord::Schema.define(version: 2021_09_09_112650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -417,6 +417,7 @@ ActiveRecord::Schema.define(version: 2021_09_09_105025) do
     t.datetime "updated_at", null: false
     t.integer "priority", default: 2
     t.integer "guests", default: 999
+    t.bigint "crm_id"
   end
 
   create_table "friendly_id_slugs", id: :serial, force: :cascade do |t|
