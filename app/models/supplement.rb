fa7ba69045592f03c79225d2f658b797c63754bf
Fuad Khan <fuad.khan@thedevden.co>
2021-09-28 14:46:32 +0500

    Add supplement and linked supplement model and its associations in lodging and room rate models

---
+++ app/models/supplement.rb
@@ -0,0 +1,26 @@
+class Supplement < ApplicationRecord
+  belongs_to :lodging #parent accommodations
+  has_many :linked_supplements
+  # has_many :linked_accommodations, through: :linked_supplements, source: :supplementable, source_type: 'Accommodation'
+  # has_many :linked_room_rates, through: :linked_supplements, source: :supplementable, source_type: 'RoomRate'
+
+  translates :name, :description
+  globalize_accessors
+
+  enum supplement_type: {
+    optional: 0,
+    mandatory: 1
+  }
+
+  enum rate_type: {
+    'Per Piece': 0,
+    'Per Piece Per Day': 1,
+    'Per Piece Per Night': 2,
+    'Per Person': 3,
+    'Per Person Per Day': 4,
+    'Per Person Per Night': 5,
+    'Per Stay': 6,
+    'Per Stay Per Day': 7,
+    'Per Stay Per Night': 8,
+  }
+end
