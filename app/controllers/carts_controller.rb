e0cb38fd57a838356bc39f554dca4c125339764f
Kawish Hussain <kawish791@gmail.com>
2022-01-28 16:39:26 +0500

    Add form and save functionality to get children age from reservation checkout page

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -6,6 +6,8 @@ class CartsController < ApplicationController
 
   def show
     @guest_centric = @booking.reservations.guest_centric.present?
+    @reservations = @booking.reservations.unexpired
+    @reservations.map { |reservation| reservation.children.times.each { reservation.guest_details.build } }
     @booking.build_user(creation_status: :without_login) unless @booking.user.present?
   end
 
@@ -70,7 +72,7 @@ class CartsController < ApplicationController
       params.require(:booking).permit(
         :in_cart,
         user_attributes: [:id, :first_name, :last_name, :email, :password, :password_confirmation, :creation_status, :country_id, :city, :zipcode, :address, :phone, :skip_validations, :language],
-        reservations_attributes: [:id, :booking_id, :in_cart, :skip_data_posting]
+        reservations_attributes: [:id, :booking_id, :in_cart, :skip_data_posting, guest_details_attributes: [:id, :age]],
       )
     end
 
