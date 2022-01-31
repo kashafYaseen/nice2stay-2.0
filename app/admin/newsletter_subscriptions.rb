a953507d6d390d14eebb1d3bf8b0f43e8f9c1c65
asadsarfraz2347 <asadsarfraz234@gmail.com>
2022-01-31 17:27:20 +0500

    N2S-175: fix index and new for NewsletterSubscription in active admin

--- app/admin/newsletter_subscriptions.rb
+++ app/admin/newsletter_subscriptions.rb
@@ -1,3 +1,24 @@
 ActiveAdmin.register NewsletterSubscription do
   permit_params :email, :language
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :email
+    column :language
+    column :created_at
+    column :updated_at
+
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :email
+      f.input :language
+    end
+    f.actions
+  end
 end
