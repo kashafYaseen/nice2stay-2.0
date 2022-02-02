250fa769328b7d7ca49f7f6253e43ea5cfff4b83
Kawish Hussain <kawish791@gmail.com>
2022-02-02 15:00:31 +0500

    N2S-223: Add enum guest_type

--- app/models/guest_detail.rb
+++ app/models/guest_detail.rb
@@ -1,3 +1,9 @@
 class GuestDetail < ApplicationRecord
   belongs_to :reservation
+
+  # Values based on CRM class and table names
+  enum guest_type: {
+    child: 'ChildDetail',
+    person: 'PersonDetail',
+  }
 end
