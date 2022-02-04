e148edb5e9c1be7718fee263ac21945a96ad5182
abdullah <abdullah.zafar@devden.io>
2022-02-04 12:17:32 +0500

    N2S-240: add place_category_name in place_serializer

--- app/serializers/api/v2/place_serializer.rb
+++ app/serializers/api/v2/place_serializer.rb
@@ -2,7 +2,7 @@ class Api::V2::PlaceSerializer
   include FastJsonapi::ObjectSerializer
   attributes  :id, :address, :spotlight, :header_dropdown, :short_desc,
               :short_desc_nav, :images, :latitude, :longitude, :country_id,
-              :region_id, :place_category_id, :created_at, :updated_at, :publish,
+              :region_id, :place_category_id, :place_category_name, :created_at, :updated_at, :publish,
               :details, :description, :name, :slug
 
   attribute :distance, if: proc { |place, params| params.present? && params[:lodging].present? } do |place, params|
