4a443fae1ec6c2e41c3a1e9332008c3f347c121f
asadsarfraz2347 <asadsarfraz234@gmail.com>
2022-02-16 11:05:02 +0500

    N2S-255: change condition in serializer to send cleaning cost in lodging cumulative price

--- app/serializers/api/v2/cumulative_price_serializer.rb
+++ app/serializers/api/v2/cumulative_price_serializer.rb
@@ -6,6 +6,10 @@ class Api::V2::CumulativePriceSerializer
     lodging.belongs_to_channel?
   end
 
+  attribute :cleaning_cost do |lodging, params|
+    lodging.cleaning_cost_for((params[:adults].to_i + params[:children].to_i), params[:nights])
+  end
+
   attributes :room_rates, if: proc { |lodging, params| lodging.belongs_to_channel? && !params[:accom_listing] } do |lodging, params|
     Api::V2::RoomRateSerializer.new(lodging.room_rates.select { |room_rate| room_rate.publish && room_rate.rate_enabled }, { params: params })
   end
@@ -18,9 +22,5 @@ class Api::V2::CumulativePriceSerializer
   attribute :mandatory_supplements, if: proc { |lodging| !lodging.belongs_to_channel? } do |lodging, params|
     supplements = lodging.as_standalone? ? lodging.supplements : lodging.linked_child_supplements
     Api::V2::SupplementSerializer.new(supplements.applied_lodgings_supplements(params[:check_in], params[:check_out], params[:adults].to_i, params[:children].to_i, ((lodging.as_standalone? && :lodging) || :linked_lodgings)).mandatory.published, params: params)
-  end
-
-  attribute :cleaning_cost, if: proc { |lodging| !lodging.belongs_to_channel? } do |lodging, params|
-    lodging.cleaning_cost_for((params[:adults].to_i + params[:children].to_i), params[:nights])
   end
 end
