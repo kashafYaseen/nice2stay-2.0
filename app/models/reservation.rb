6047412a9747ecf9f5c516dd8685980bd13e8ef7
abdullah <abdullah.zafar@devden.io>
2022-04-27 16:11:06 +0500

    N2S-366: add security_deposit of each reservation against booking

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -183,6 +183,10 @@ class Reservation < ApplicationRecord
     is_managed_by_n2s? ? 0 : cleaning_cost.to_f
   end
 
+  def security_deposit_on_location
+    !include_deposit? ? 0 : security_deposit.to_f
+  end
+
   def self.arrival_status
     _arrivals = {}
     joins(lodging: { region: :country }).select("in_cart, check_in, countries.id").where("in_cart = ? and check_in >= ? and request_status = ?", false, Date.today, 1).group(:"countries.id").group_by_month(:check_in).count("id").map { |arrival| _arrivals[arrival.first[0]].present? ? _arrivals[arrival.first[0]] << [arrival.first[1], arrival.last] : _arrivals[arrival.first[0]] = [[arrival.first[1], arrival.last]] }
