f1cc280ebc294d71c52a5a4543ae511c522d04e7
hunaid-nawaz <hunaid.nawaz@gmail.com>
2022-07-21 12:35:57 +0500

    N2S-546: Updation in lodging and lodging category serializers to add count in api response

--- app/serializers/api/v2/lodging_serializer.rb
+++ app/serializers/api/v2/lodging_serializer.rb
@@ -6,7 +6,8 @@ class Api::V2::LodgingSerializer
              :images, :average_rating, :created_at, :updated_at, :highlight_1,
              :highlight_2, :highlight_3, :beds, :baths, :channel, :particularities_text,
              :open_gds_property_id, :open_gds_accommodation_id, :including_text,
-             :setting, :quality, :interior, :service, :communication, :realtime_availability
+             :setting, :quality, :interior, :service, :communication, :realtime_availability,
+             :lodging_category_id
 
   attribute :total_reviews do |lodging|
     lodging.all_reviews.count
