609eb64b63b2bcd927af6db22cebc6d21c9cd2c8
hunaid-nawaz <hunaid.nawaz@gmail.com>
2022-10-24 11:04:29 +0500

    N2S-700: add logs in social login controller

--- app/controllers/api/v2/socials_controller.rb
+++ app/controllers/api/v2/socials_controller.rb
@@ -4,6 +4,8 @@ class Api::V2::SocialsController < Api::V2::ApiController
       user = Google::People.new(social_params[:authorization_code]).config_user
     end
 
+    puts "User found ---------------#{user&.id}"
+
     render json: Api::V2::UserSerializer.new(user, { params: { auth_token: true } }).serialized_json, status: :created
   end
 
