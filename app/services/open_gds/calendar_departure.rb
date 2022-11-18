64f28107be94f7578f7d32be54b4f8f3c0128594
hunaid-nawaz <hunaid.nawaz@gmail.com>
2022-11-18 16:03:10 +0500

    N2S-760: fix the calendar biuld and departure issue for opengds

--- app/services/open_gds/calendar_departure.rb
+++ app/services/open_gds/calendar_departure.rb
@@ -1,15 +1,21 @@
 class OpenGds::CalendarDeparture
-  attr_reader :room_rate,
+  attr_reader :lodging,
               :params,
+              :rate_plan,
               :uri
 
-  def self.call(room_rate:, params:)
-    new(room_rate: room_rate, params: params).call
+  def self.call(lodging:, params:)
+    new(lodging: lodging, params: params).call
   end
 
-  def initialize(room_rate:, params:)
-    @room_rate = room_rate
+  def self.fetch(lodging:, params:)
+    new(lodging: lodging, params: params).fetch
+  end
+
+  def initialize(lodging:, params:)
     @params = params
+    @lodging = lodging
+    @rate_plan = get_rate_plan
     @uri = URI.parse("https://api.opengds.com/core/v1/acc-status/calendar-depart?#{query_params}")
   end
 
@@ -19,6 +25,37 @@ class OpenGds::CalendarDeparture
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     JSON.parse(http.request(request).body)
+  end
+
+  def fetch
+    room_rate_ids = rate_plan.room_rate_ids
+    check_in_availabilities = Availability.where(room_rate_id: room_rate_ids, available_on: params[:check_in])
+    response = []
+    check_in_availabilities.each do |check_in_availability|
+      prices = {}
+      dates_combinations(check_in_availability).each do |dates_combination|
+        room_rate_params = params_based_on dates_combination
+        next if room_rate_params.blank?
+
+        price_details = check_in_availability.room_rate.price_details(values: room_rate_params, daily_rate: true, calendar_departure: true)
+        next unless price_details[:valid]
+
+        price_details[:rates_with_dates].each do |rate_with_date|
+          next if prices[rate_with_date[:date]].present?
+          prices[rate_with_date[:date]] = { date: rate_with_date[:date], rate: rate_with_date[:rate], ctd: rate_with_date[:date] != dates_combination[:check_out].to_date }
+        end
+
+        prices[dates_combination[:check_out].to_date] = { date: dates_combination[:check_out].to_date, rate: nil, ctd: false } unless prices[dates_combination[:check_out].to_date].present?
+
+      end
+
+      next if prices.blank?
+      response << prices.keys.map { |res| prices[res] }.sort_by { |r| r[:date] }
+    end
+
+    response = response.flatten.group_by{|r| r[:date]}.map{|key, value| value.find{|val| val[:rate] == value.pluck(:rate).min}}
+    response[-1][:rate] = nil
+    response
   end
 
   private
@@ -31,11 +68,11 @@ class OpenGds::CalendarDeparture
     end
 
     def accommodation_id
-      room_rate.open_gds_accommodation_id
+      lodging.lodging_children.pluck(:open_gds_accommodation_id)
     end
 
     def rate_id
-      room_rate.open_gds_rate_id
+      rate_plan.open_gds_rate_id
     end
 
     def check_in
@@ -49,12 +86,25 @@ class OpenGds::CalendarDeparture
     def children
       return unless params[:children].present?
 
-      child_rate = room_rate.child_rates.order(rate: :desc).first
-      @child_category = child_rate&.open_gds_category || 1
-      ", {\"#{@child_category}\": #{params[:children]}}"
+      child_rate = rate_plan.child_rates.order(rate: :desc).first
+      child_category = child_rate&.open_gds_category || 1
+      ", {\"#{child_category}\": #{params[:children]}}"
     end
 
     def credentials
       "privkey=#{ENV['OPENGDS_PRIV_KEY']}&apikey=#{ENV['OPENGDS_API_KEY']}"
+    end
+
+    def get_rate_plan
+      lodging.rate_plans.find_by(id: params[:rate_plan_id]) || lodging.rate_plans.first
+    end
+
+    def params_based_on dates_combination
+      [dates_combination[:check_in].to_s, dates_combination[:check_out].to_s, params[:adults] || 1, params[:children], false]
+    end
+
+    def dates_combinations(check_in_availability)
+      return [] unless check_in_availability.minimum_stay.present?
+      check_in_availability.minimum_stay.map { |min_stay| { check_in: check_in_availability.available_on, check_out: check_in_availability.available_on + min_stay.to_i } }
     end
 end
