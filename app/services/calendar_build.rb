a43e6b19cb512cfd18908e8cc500b0273afca7c1
hunaid-nawaz <hunaid.nawaz@gmail.com>
2022-11-28 16:42:31 +0500

    N2S-777: fix the calendar build api

--- app/services/calendar_build.rb
+++ app/services/calendar_build.rb
@@ -11,19 +11,6 @@ class CalendarBuild
   end
 
   def call
-    @lodging.open_gds? ? open_gds_accommodations_calendar : non_open_gds_accommodations_calendar
-  end
-
-  def open_gds_accommodations_calendar
-    response = []
-    lodging.lodging_children.pluck(:open_gds_accommodation_id).reject(&:blank?).sort.each do |open_gds_accommodation_id|
-      response <<  OpenGds::CalendarBuild.call(lodging: lodging, params: params, accommodation_id: open_gds_accommodation_id)
-    end
-
-    response.flatten.group_by{ |r| r["date"] }.reject{ |key, value| key.nil? }.map{ |key, value| value.find{ |val| val["rate"] == value.pluck("rate").min }}
-  end
-
-  def non_open_gds_accommodations_calendar
     response =  []
     rules = lodging.rules_active(check_in, check_out)
     availabilities = lodging.availabilities.for_range(check_in, check_out)
