f70cb92f29d0a0e7b09309587ffcba64c74b1ea5
hunaid-nawaz <hunaid.nawaz@gmail.com>
2022-12-02 15:40:32 +0500

    N2S-809: Update save amenities details add crm_id in it

--- app/services/save_amenity_details.rb
+++ app/services/save_amenity_details.rb
@@ -8,7 +8,7 @@ class SaveAmenityDetails
 
   def initialize(params)
     @params = params
-    @amenity = Amenity.find_or_initialize_by(slug: amenity_params[:slug])
+    @amenity = Amenity.find_by(slug: amenity_params[:slug]) || Amenity.find_by(crm_id: amenity_params[:crm_id]) || Amenity.new(crm_id: amenity_params[:crm_id])
   end
 
   def call
@@ -60,6 +60,7 @@ class SaveAmenityDetails
       params.require(:amenity).permit(
         :slug,
         :name,
+        :crm_id,
         :filter_enabled,
         :hot,
         :icon,
