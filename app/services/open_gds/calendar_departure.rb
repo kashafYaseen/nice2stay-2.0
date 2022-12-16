886ca0d6516425563b4bdf544f21e20ca61e627a
hunaid-nawaz <hunaid.nawaz@gmail.com>
2022-12-16 11:35:02 +0500

    N2S-837: call the api of open_gds again with the defautl adults 2 if data will empty

--- app/services/open_gds/calendar_departure.rb
+++ app/services/open_gds/calendar_departure.rb
@@ -2,38 +2,47 @@ class OpenGds::CalendarDeparture
   attr_reader :lodging,
               :params,
               :rate_plan,
-              :uri
+              :uri,
+              :default_adults
 
-  def self.call(lodging:, params:)
-    new(lodging: lodging, params: params).call
+  def self.call(params:, lodging:, default_adults: nil)
+    new(params: params, lodging: lodging, default_adults: default_adults).call
   end
 
-  def initialize(lodging:, params:)
+  def initialize(params:, lodging:, default_adults:)
     @params = params
     @lodging = lodging
     @rate_plan = get_rate_plan
+    @default_adults = default_adults
     @uri = URI.parse("https://api.opengds.com/core/v1/acc-status/calendar-depart?#{query_params}")
   end
 
   def call
+    return if rate_plan.blank?
+
+    result = []
     request = Net::HTTP::Get.new(uri.request_uri)
     request.content_type = 'application/x-www-form-urlencoded; charset=UTF-8'
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     response = JSON.parse(http.request(request).body)
-    parse_data(response)
+    if response.is_a?(Hash) # IF there will be hash in response it means there is an error occured in the request so we call the api with default adults 2
+      return OpenGds::CalendarBuild.call(lodging: lodging, params: params, default_adults: 2)
+    else
+      return parse_data(response)
+    end
   end
 
   private
     def parse_data(response)
-      return [] if response.is_a?(Hash) # IF there will be hash in response it means there is an error occured in the request
-
       result = response.flatten.group_by{ |r| r["date"] }.reject{ |key, value| key.nil? }.map do |key, value|
         minimum_value = value.pluck("rate").reject(&:blank?).min
         value.find{ |val| val["rate"] == minimum_value }
       end
 
       result.reject{|r| r["rate"].blank?}
+      result << { error: I18n.t('open_gds.error') } if default_adults
+      result
     end
 
     def query_params
@@ -45,6 +54,8 @@ class OpenGds::CalendarDeparture
     end
 
     def accommodation_id
+      # total_occupancy = params[:adults].to_i + params[:children].to_i
+      # rate_plan.room_rates.select{ |rr | rr.adults >= total_occupancy }.map(&:open_gds_accommodation_id).reject(&:blank?).sort.presence || rate_plan.room_ratesfirst.try(:open_gds_accommodation_id)
       rate_plan.room_rates.map{|rr| rr.child_lodging.try(:open_gds_accommodation_id)}.reject(&:blank?).sort
     end
 
@@ -57,7 +68,8 @@ class OpenGds::CalendarDeparture
     end
 
     def occupancy
-      "[#{params[:adults]}#{children}]"
+      adults = default_adults || params[:adults]
+      "[#{adults}#{children}]"
     end
 
     def children
