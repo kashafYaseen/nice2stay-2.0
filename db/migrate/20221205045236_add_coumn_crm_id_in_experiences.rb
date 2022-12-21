12eb25d684868ceb675f4f2ed875087b2f32b3b6
hunaid-nawaz <hunaid.nawaz@gmail.com>
2022-12-21 11:40:14 +0500

    N2S-710: update the migration for experience table crm-id

--- db/migrate/20221205045236_add_coumn_crm_id_in_experiences.rb
+++ db/migrate/20221205045236_add_coumn_crm_id_in_experiences.rb
@@ -1,5 +1,5 @@
 class AddCoumnCrmIdInExperiences < ActiveRecord::Migration[5.2]
   def change
-    add_column :experiences, :crm_id, :integer
+    add_column :experiences, :crm_id, :integer unless Experience.column_names.include?("crm_id")
   end
 end
