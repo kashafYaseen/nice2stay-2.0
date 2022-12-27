ed8cb0b7fabaefa36b35ef1c3ee8316d4fdcfdf3
Abdullah Zafar <abdzafar336@gmail.com>
2022-12-27 14:16:35 +0500

    N2S-852: add splited_rates in room_rate serializer

--- app/serializers/api/v2/room_rate_serializer.rb
+++ app/serializers/api/v2/room_rate_serializer.rb
@@ -1,6 +1,6 @@
 class Api::V2::RoomRateSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :default_rate, :calculated_price, :dynamic_price, :price_valid, :price_errors, :default_booking_limit
+  attributes :id, :default_rate, :calculated_price, :splited_rates, :dynamic_price, :price_valid, :price_errors, :default_booking_limit
 
   attribute :min_booking_limit, if: proc { |room_rate, params| params.present? && params[:check_in].present? && params[:check_out].present? } do |room_rate, params|
     room_rate.minimum_booking_limit(params)
