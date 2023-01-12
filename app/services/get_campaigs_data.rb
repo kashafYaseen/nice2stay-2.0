0f509a170678fab12f9bbef59e677393c7687034
hunaid-nawaz <hunaid.nawaz@gmail.com>
2023-01-12 10:00:04 +0500

    N2S-893: add service to get campaign data on the basis of check

---
+++ app/services/get_campaigs_data.rb
@@ -0,0 +1,24 @@
+class GetCampaigsData
+  include Rails.application.routes.url_helpers
+
+  attr_reader :type
+  attr_reader :locale
+
+  def self.call(type, locale)
+    self.new(type, locale).call
+  end
+
+  def initialize(type, locale)
+    @type = type
+    @locale = locale
+  end
+
+  def call
+    Campaign.search('*', { where: { "#{type}": true }, load: false}).map { |campaign| {
+      name: campaign.send("title_#{locale}"),
+      id: campaign.id,
+      type: 'campaign',
+      url: campaign.send("redirect_url_#{locale}")
+    }}
+  end
+end
