dc698cb5ec3f0a5979de1f2e600f942c01e22fbb
hunaid-nawaz <hunaid.nawaz@gmail.com>
2023-01-16 16:40:31 +0500

    fix: fix the undefine column params issue in CacheLodgingsJob

--- app/jobs/cache_lodgings_job.rb
+++ app/jobs/cache_lodgings_job.rb
@@ -6,7 +6,7 @@ class CacheLodgingsJob < ApplicationJob
     SearchAnalytic.all.each do |search_analytic|
       search_params = search_analytic.params.with_indifferent_access[:lodgings]
       next if search_params.blank?
-      next if search_params[:check_in].present? && params[:check_in].to_date < Date.today
+      next if search_params[:check_in].present? && search_params[:check_in].to_date < Date.today
       if search_params[:action] == 'index'
         dates = dates_by_months(search_params)
         ::V2::SearchLodgings.call(search_params.clone.merge(months_date_range: dates, flexible_dates: flexible_dates(dates, search_params)), search_analytic, nil)
