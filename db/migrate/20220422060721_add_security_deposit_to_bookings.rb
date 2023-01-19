6087dc4ff0b994c85b77c322c9909054a18ee2f8
hunaid-nawaz <hunaid.nawaz@gmail.com>
2023-01-19 17:26:57 +0500

    N2S-921: check column exist in table or not

--- db/migrate/20220422060721_add_security_deposit_to_bookings.rb
+++ db/migrate/20220422060721_add_security_deposit_to_bookings.rb
@@ -1,6 +1,8 @@
 class AddSecurityDepositToBookings < ActiveRecord::Migration[5.2]
   def change
     add_column :bookings, :security_deposit_payment, :float
-    add_column :bookings, :security_deposit_payment_mollie_id, :string
+    unless column_exists? :bookings, :security_deposit_payment_mollie_id
+      add_column :bookings, :security_deposit_payment_mollie_id, :string
+    end
   end
 end
