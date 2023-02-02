0d65e54b8ee560091c3964175259b50d8f039fe3
hunaid-nawaz <hunaid.nawaz@gmail.com>
2023-02-02 10:06:12 +0500

    N2S-921: check column exists in db or not

--- db/migrate/20210909112650_add_crm_id_field_in_experiences.rb
+++ db/migrate/20210909112650_add_crm_id_field_in_experiences.rb
@@ -1,5 +1,7 @@
 class AddCrmIdFieldInExperiences < ActiveRecord::Migration[5.2]
   def change
-    add_column :experiences, :crm_id, :bigint
+    unless column_exists? :experiences, :crm_id
+      add_column :experiences, :crm_id, :bigint
+    end
   end
 end
