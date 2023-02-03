3fec9bdd02f97b46636c7d7b36aabe8fa1691be0
hunaid-nawaz <hunaid.nawaz@gmail.com>
2023-02-03 17:32:44 +0500

    N2S-987: update the default max price for filter

--- app/helpers/lodgings_helper.rb
+++ app/helpers/lodgings_helper.rb
@@ -8,7 +8,7 @@ module LodgingsHelper
   end
 
   def render_max_price(price)
-    price || 1500
+    price || 5000
   end
 
   def render_lodgings_count_for (lodgings, key, filter_name, total_lodgings)
