ce3fdd532863693c11201146e3a05466b8621f4c
Kashaf Yaseen <yaseenkashaf9@gmail.com>
2023-07-17 13:40:55 +0500

    add missing attributes in country's and region's serializers

--- app/serializers/crm/v1/region_serializer.rb
+++ app/serializers/crm/v1/region_serializer.rb
@@ -1,8 +1,11 @@
 class Crm::V1::RegionSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name, :content
+  attributes :id, :name_en, :name_nl, :slug_en, :slug_nl, :title_en, :title_nl, :content_en, :content_nl,
+              :meta_title_en, :meta_title_nl, :published, :short_desc, :country_id, :villas_desc,
+              :apartment_desc, :bb_desc
 
   attributes :region_country do |region|
-    region.country.try(:name)
+    region.country.try(:name_en)
   end
+
 end
