e4e2b4e1ba074c46ee6e52adac14dc2f68c79cde
Kashaf Yaseen <yaseenkashaf9@gmail.com>
2023-07-25 17:22:56 +0500

    Remove unnecessary code and added additional line after return statement

--- app/controllers/crm/v1/admin_user/sessions_controller.rb
+++ app/controllers/crm/v1/admin_user/sessions_controller.rb
@@ -1,9 +1,9 @@
 class Crm::V1::AdminUser::SessionsController < Crm::V1::ApiController
-  before_action :authenticate, only: [:update]
 
   def create
     @user = User.authenticate(email: params[:email], password: params[:password])
     return invalid_credentials unless @user.present?
+
     @user.regenerate_auth_token
     render json: Crm::V1::UserSerializer.new(@user, { params: { auth_token: true } }).serialized_json, status: :created
   end
