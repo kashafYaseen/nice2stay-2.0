53809b4d320607711da6dd08ad65566fc4f61437
Kashaf Yaseen <yaseenkashaf9@gmail.com>
2023-07-31 11:48:07 +0500

    Add token_expires_at column in admin-user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_17_071048) do
+ActiveRecord::Schema.define(version: 2023_07_31_061800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2023_07_17_071048) do
     t.string "image"
     t.string "first_name"
     t.string "last_name"
+    t.string "token_expires_at"
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
