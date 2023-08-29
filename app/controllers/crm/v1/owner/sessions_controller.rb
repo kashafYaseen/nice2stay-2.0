d7e86340b75c0b9262d37cb4449c8e8872bd8bbc
kashafYaseen <yaseenkashaf9@gmail.com>
2023-08-29 15:05:36 +0500

    Refactor: Remove extra spaces and switched to single quotes

--- app/controllers/crm/v1/owner/sessions_controller.rb
+++ app/controllers/crm/v1/owner/sessions_controller.rb
@@ -1,17 +1,15 @@
 class Crm::V1::Owner::SessionsController < Crm::V1::Owner::ApiController
-
   before_action :set_owner
 
-
   def create
-    if (@owner&.invitation_status === "Accepted")
+    if (@owner&.invitation_status === 'Accepted')
       owner = Owner.authenticate(email: params[:email], password: params[:password])
       return invalid_credentials unless owner.present?
 
       owner.regenerate_auth_token
       render json: Crm::V1::OwnerSerializer.new(owner, { params: { auth_token: true } }).serialized_json, status: :created
 
-    elsif (@owner&.invitation_status === "Pending")
+    elsif (@owner&.invitation_status === 'Pending')
 
       render json: { errors: 'Please Accept the Invitation first' }, status: :unprocessable_entity
     else
@@ -21,9 +19,7 @@ class Crm::V1::Owner::SessionsController < Crm::V1::Owner::ApiController
   end
 
   private
-
   def set_owner
     @owner = Owner.find_by(email: params[:email])
   end
-
 end
